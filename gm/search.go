03f5268ec54b08e2f74e011dd5b7e5207774f306
Raul Durand <raul.durand@gmail.com>
2015-02-16 13:37:25 +1000

    Bins structure and methods finished. needs testing

--- gm/search.go
+++ gm/search.go
@@ -4,7 +4,11 @@
 
 package gm
 
-import "github.com/cpmech/gosl/utl"
+import (
+	"math"
+
+	"github.com/cpmech/gosl/utl"
+)
 
 // HashPoint returns a unique id of a point
 func HashPoint(x, y, z float64) int {
@@ -27,31 +31,9 @@ func HashPointC(c []float64) int {
 	return HashPoint(x, y, z)
 }
 
-/*
-//    FAST_32_hash
-//    A very fast 2D hashing function.  Requires 32bit support.
-//
-//    The hash formula takes the form....
-//    hash = mod( coord.x * coord.x * coord.y * coord.y, SOMELARGEFLOAT ) / SOMELARGEFLOAT
-//    We truncate and offset the domain to the most interesting part of the noise.
-//
-vec4 FAST_32_hash( vec2 gridcell )
-{
-//    gridcell is assumed to be an integer coordinate
-const vec2 OFFSET = vec2( 26.0, 161.0 );
-const float DOMAIN = 71.0;
-const float SOMELARGEFLOAT = 951.135664;
-vec4 P = vec4( gridcell.xy, gridcell.xy + 1.0.xx );
-P = P - floor(P * ( 1.0 / DOMAIN )) * DOMAIN;    //    truncate the domain
-P += OFFSET.xyxy;                                //    offset to interesting part of the noise
-P *= P;                                          //    calculate and return the hash
-return fract( P.xzxz * P.yyww * ( 1.0 / SOMELARGEFLOAT.x ).xxxx );
-}
-*/
-
 type BinEntry struct {
-	Id  int // object Id
-	Idx int // index of bin
+	Id int       // object Id
+	x  []float64 // entry coordinate
 }
 
 type Bin struct {
@@ -60,31 +42,51 @@ type Bin struct {
 }
 
 type Bins struct {
-	Ndim    int       // space dimension
-	Xi      []float64 // [ndim] left/lower-most point
-	Xf      []float64 // [ndim] right/upper-most point
-	L       []float64 // [ndim] whole box lengths
-	N       []int     // [ndim] number of divisions
-	S       float64   // size of bins
-	All     []*Bin    // [nbins] all bins
-	Idx2bin []int     // maps idx of bin to item in All
-	tmp     []int     // [ndim] temporary (auxiliary) slice
+	Ndim int       // space dimension
+	Xi   []float64 // [ndim] left/lower-most point
+	Xf   []float64 // [ndim] right/upper-most point
+	L    []float64 // [ndim] whole box lengths
+	N    []int     // [ndim] number of divisions
+	S    float64   // size of bins
+	All  []*Bin    // [nbins] all bins
+	tmp  []int     // [ndim] temporary (auxiliary) slice
 }
 
 // xi   -- [ndim] initial positions
 // l    -- [ndim] whole box lengths
-// ndiv -- number of divisions
-func (o *Bins) Init(xi, l []float64, ndiv int) (err error) {
+// ndiv -- number of divisions for the maximun length
+func (o *Bins) Init(xi, xf []float64, ndiv int) (err error) {
 	o.Ndim = len(xi)
-	o.Xi, o.L = xi, l
-	if len(xi) != len(l) || len(l) < 2 || len(l) > 3 {
+	o.Xi = xi
+	o.Xf = xf
+	if len(xi) != len(xf) || len(xi) < 2 || len(xi) > 3 {
 		return utl.Err("sizes of xi and l must be the same and equal to either 2 or 3")
 	}
-	o.Xf = make([]float64, o.Ndim)
+
+	o.L = make([]float64, o.Ndim)
+	o.N = make([]int, o.Ndim)
+
 	for k := 0; k < o.Ndim; k++ {
-		o.Xf[k] = o.Xi[k] + o.L[k]
-		o.N[k] = int(o.L[k])/ndiv + 1
+		o.L[k] = o.Xf[k] - o.Xi[k]
 	}
+
+	// maximun lenght
+	lmax := math.Max(o.L[0], o.L[1])
+	if o.Ndim == 3 {
+		lmax = math.Max(lmax, o.L[2])
+	}
+	o.S = lmax / float64(ndiv)
+
+	// number of divisions
+	nbins := 1
+	for k := 0; k < o.Ndim; k++ {
+		o.N[k] = int(o.L[k]/o.S) + 1
+		nbins *= o.N[k]
+	}
+
+	o.All = make([]*Bin, nbins)
+	o.tmp = make([]int, o.Ndim)
+
 	return
 }
 
@@ -94,42 +96,48 @@ func (o *Bins) Append(x []float64, id int) (err error) {
 		return utl.Err("point %v is out of range", x)
 	}
 	bin := o.FindBinByIndex(idx)
-	if len(bin) == 0 {
-		entrynumber := len(o.Entries)
-		o.Entries = append(o.Entries, []*BinEntry{&BinEntry{id, idx}})
-		o.Idx2entry[idx] = entrynumber
-		return
+	if bin == nil {
+		return utl.Err("bin index %v is out of range", idx)
 	}
 
-	entry.Id = id
+	entry := BinEntry{id, x}
+	bin.Entries = append(bin.Entries, &entry)
 	return
 }
 
 func (o *Bins) Clear() {
-	o.Entries = make([]*BinEntry, 0)
-	o.Idx2entry = make([]int, 0)
+	o.All = make([]*Bin, 0)
 }
 
 func (o Bins) FindBinByIndex(idx int) *Bin {
-	if idx < 0 || idx >= len(o.Idx2bin) {
+	if idx < 0 || idx >= len(o.All) {
 		return nil
 	}
-	k := o.Idx2bin[idx]
-	return o.All[k]
+	return o.All[idx]
 }
 
-func (o Bins) FindByCoords(x []float64) *BinEntry {
+func (o Bins) Find(x []float64) *BinEntry {
 	idx := o.CalcIdx(x)
 	if idx < 0 {
 		return nil // out-of-range
 	}
-	return o.FindByIndex(idx)
+	bin := o.FindBinByIndex(idx)
+	dmin := math.MaxFloat64
+	var entry, closest *BinEntry
+	for _, entry = range bin.Entries {
+		var d float64
+		for k := 0; k < o.Ndim; k++ {
+			d += math.Pow(x[k]-entry.x[k], 2)
+		}
+		if d < dmin {
+			dmin = d
+			closest = entry
+		}
+	}
+	return closest
 }
 
-func (o Bins) FindCloseTo(x []float64) *BinEntry {
-	return nil
-}
-
+// CalcIdx calculates the bin index where the point x is
 // returns -1 if out-of-range
 func (o Bins) CalcIdx(x []float64) int {
 	for k := 0; k < o.Ndim; k++ {
