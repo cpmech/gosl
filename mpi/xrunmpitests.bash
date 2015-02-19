d2d019eb5f732608abf943c2f60379ab7c30b546
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-02-19 19:49:40 +1000

    mpi test script updated

--- mpi/xrunmpitests.bash
+++ mpi/xrunmpitests.bash
@@ -2,6 +2,5 @@
 
 tests="t_mpi01_main t_mpi02_main t_mpi03_main t_mpi04_main"
 for t in $tests; do
-    go build -o /tmp/gosl/$t "$t".go
-    mpirun -np 3 /tmp/gosl/$t
+    go build -o /tmp/gosl/$t "$t".go && mpirun -np 3 /tmp/gosl/$t
 done
