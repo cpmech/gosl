c5552815a2f4699c73f16e44bbc45963ffdac26a
Raul Durand <raul.durand@gmail.com>
2015-02-27 16:06:26 +1000

    trapezoidal and simpsom rules updated

--- num/quadrature.go
+++ num/quadrature.go
@@ -66,6 +66,69 @@ func (o *Trap) Next() float64 {
 
 func (o *Trap) Integrate() (float64, error) {
 	jmax := 20
+	var olds float64
+	for j := 0; j < jmax; j++ {
+		o.s = o.Next()
+		if j > 5 {
+			if math.Abs(o.s-olds) < o.eps*math.Abs(olds) || (o.s == 0 && olds == 0) {
+				return o.s, nil
+			}
+		}
+		olds = o.s
+	}
+	return 0, fmt.Errorf("Error - achieved maximun number of iterations (n=%d)", jmax)
+}
+
+// Then the Simp structure is derived from this as follows:
+type Simp struct {
+	n    int     // Current level of refinement.
+	a, b float64 // limits
+	s    float64 // current value of the integral
+	eps  float64 // presicion
+	// Limits of integration and current value of integral.
+	f fcn
+}
+
+func (o *Simp) Init(f fcn, a, b, eps float64) {
+	o.n = 0
+	o.f = f
+	o.a = a
+	o.b = b
+	o.eps = eps
+}
+
+// Next returns the nth stage of refinement of the extended trapezoidal rule. On the first call (n=1),
+// R b the routine returns the crudest estimate of a f .x/dx. Subsequent calls set n=2,3,... and
+// improve the accuracy by adding 2 n-2 additional interior points.
+func (o *Simp) Next() float64 {
+	var x, sum, del float64
+	var it, j, tnm int
+	o.n += 1
+	if o.n == 1 {
+		o.s = 0.5 * (o.b - o.a) * (o.f(o.a) + o.f(o.b))
+		return o.s
+	} else {
+		for it, j = 1, 1; j < o.n-1; j++ {
+			it *= 2
+		}
+		tnm = it
+		del = (o.b - o.a) / float64(tnm)
+
+		// This is the spacing of the points to be added.
+		x = o.a + 0.5*del
+
+		for sum, j = 0.0, 0; j < it; j, x = j+1, x+del {
+			sum += o.f(x)
+		}
+		o.s = 0.5 * (o.s + (o.b-o.a)*sum/float64(tnm))
+
+		// This replaces s by its refined value.
+		return o.s
+	}
+}
+
+func (o *Simp) Integrate() (float64, error) {
+	jmax := 20
 	var s, st, ost, os float64
 	for j := 0; j < jmax; j++ {
 		st = o.Next()
--- num/t_quadrature_test.go
+++ num/t_quadrature_test.go
@@ -28,20 +28,36 @@ func Test_simpson01(tst *testing.T) {
 	chk.Scalar(tst, "A", 1e-11, A, Acor)
 }
 
-func Test_trap01(tst *testing.T) {
+func Test_quad_01(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("trapz03")
+	chk.PrintTitle("quad01")
 
 	y := func(x float64) float64 {
 		return math.Sqrt(1.0 + math.Pow(math.Sin(x), 3.0))
 	}
+	var err error
+	Acor := 1.08268158558
 
+	// trapezoidal rule
 	var T Trap
 	T.Init(y, 0, 1, 1e-11)
-	A, _ := T.Integrate()
+	A, err := T.Integrate()
+	if err != nil {
+		io.Pforan(err.Error())
+	}
 	io.Pforan("A  = %v\n", A)
 	io.Pforan("n  = %v\n", T.n)
-	Acor := 1.08268158558
 	chk.Scalar(tst, "A", 1e-11, A, Acor)
+
+	// Simpson's rule
+	var S Simp
+	S.Init(y, 0, 1, 1e-09)
+	A, err = S.Integrate()
+	if err != nil {
+		io.Pforan(err.Error())
+	}
+	io.Pforan("A  = %v\n", A)
+	io.Pforan("n  = %v\n", S.n)
+	chk.Scalar(tst, "A", 1e-09, A, Acor)
 }
