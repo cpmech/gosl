0133f933854ccd5cb1240da93f97b072189ff223
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-04-15 11:01:32 +1000

    pipeline and test added

--- io/external.go
+++ io/external.go
@@ -34,3 +34,54 @@ func CopyFileOver(destination, source string) (err error) {
 	_, err = RunCmd(false, "cp", "-rf", source, destination)
 	return
 }
+
+// Pipeline strings together the given exec.Cmd commands in a similar fashion
+// to the Unix pipeline. Each command's standard output is connected to the
+// standard input of the next command, and the output of the final command in
+// the pipeline is returned, along with the collected standard error of all
+// commands and the first error found (if any).
+//
+// by Kyle Lemons
+//
+// To provide input to the pipeline, assign an io.Reader to the first's Stdin.
+func Pipeline(cmds ...*exec.Cmd) (pipeLineOutput, collectedStandardError []byte, pipeLineError error) {
+	// Require at least one command
+	if len(cmds) < 1 {
+		return nil, nil, nil
+	}
+
+	// Collect the output from the command(s)
+	var output bytes.Buffer
+	var stderr bytes.Buffer
+
+	last := len(cmds) - 1
+	for i, cmd := range cmds[:last] {
+		var err error
+		// Connect each command's stdin to the previous command's stdout
+		if cmds[i+1].Stdin, err = cmd.StdoutPipe(); err != nil {
+			return nil, nil, err
+		}
+		// Connect each command's stderr to a buffer
+		cmd.Stderr = &stderr
+	}
+
+	// Connect the output and error for the last command
+	cmds[last].Stdout, cmds[last].Stderr = &output, &stderr
+
+	// Start each command
+	for _, cmd := range cmds {
+		if err := cmd.Start(); err != nil {
+			return output.Bytes(), stderr.Bytes(), err
+		}
+	}
+
+	// Wait for each command to complete
+	for _, cmd := range cmds {
+		if err := cmd.Wait(); err != nil {
+			return output.Bytes(), stderr.Bytes(), err
+		}
+	}
+
+	// Return the pipeline output and the collected standard error
+	return output.Bytes(), stderr.Bytes(), nil
+}
