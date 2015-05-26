fca9304cd5de762099a3df2d20cdbe7956229789
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-05-26 16:15:53 +1000

    NurbsDraw fixed (old google code dependency remove)

--- tools/NurbsDraw.go
+++ tools/NurbsDraw.go
@@ -5,55 +5,56 @@
 package main
 
 import (
-    "flag"
-    "code.google.com/p/gosl/gm"
-    "code.google.com/p/gosl/utl"
-    "code.google.com/p/gosl/plt"
+	"flag"
+
+	"github.com/cpmech/gosl/gm"
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/plt"
 )
 
 func main() {
 
-    // catch errors
-    utl.Tsilent = false
-    defer func() {
-        if err := recover(); err != nil {
-            utl.PfRed("Some error has happened: %v\n", err)
-        }
-    }()
+	// default input data
+	fn := "nurbs01.msh"
+	ctrl := true
+	ids := true
+	npts := 41
 
-    // default input data
-    fn   := "nurbs01.msh"
-    ctrl := true
-    ids  := true
-    npts := 41
-
-    // parse flags
+	// parse flags
 	flag.Parse()
-	if len(flag.Args()) > 0 { fn   =          flag.Arg(0)  }
-    if len(flag.Args()) > 1 { ctrl = utl.Atob(flag.Arg(1)) }
-    if len(flag.Args()) > 2 { ids  = utl.Atob(flag.Arg(2)) }
-    if len(flag.Args()) > 3 { npts = utl.Atoi(flag.Arg(3)) }
+	if len(flag.Args()) > 0 {
+		fn = flag.Arg(0)
+	}
+	if len(flag.Args()) > 1 {
+		ctrl = io.Atob(flag.Arg(1))
+	}
+	if len(flag.Args()) > 2 {
+		ids = io.Atob(flag.Arg(2))
+	}
+	if len(flag.Args()) > 3 {
+		npts = io.Atoi(flag.Arg(3))
+	}
 
-    // print input data
-    utl.Pforan("Input data\n")
-    utl.Pforan("==========\n")
-    utl.Pfblue2("  fn   = %v\n", fn)
-    utl.Pfblue2("  ctrl = %v\n", ctrl)
-    utl.Pfblue2("  ids  = %v\n", ids)
-    utl.Pfblue2("  npts = %v\n", npts)
+	// print input data
+	io.Pforan("Input data\n")
+	io.Pforan("==========\n")
+	io.Pfblue2("  fn   = %v\n", fn)
+	io.Pfblue2("  ctrl = %v\n", ctrl)
+	io.Pfblue2("  ids  = %v\n", ids)
+	io.Pfblue2("  npts = %v\n", npts)
 
-    // load nurbss
-    fnk := utl.FnKey(fn)
-    B   := gm.ReadMsh(fnk)
+	// load nurbss
+	fnk := io.FnKey(fn)
+	B := gm.ReadMsh(fnk)
 
-    // plot
-    plt.SetForEps(0.75, 500)
-    for _, b := range B {
-        if ctrl {
-            b.DrawCtrl2D(ids)
-        }
-        b.DrawElems2D(npts, ids, "", "")
-    }
-    plt.Equal()
-    plt.Save(fnk + ".eps")
+	// plot
+	plt.SetForEps(0.75, 500)
+	for _, b := range B {
+		if ctrl {
+			b.DrawCtrl2D(ids)
+		}
+		b.DrawElems2D(npts, ids, "", "")
+	}
+	plt.Equal()
+	plt.Save(fnk + ".eps")
 }
