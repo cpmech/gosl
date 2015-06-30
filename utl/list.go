08c0f083cd9edc2ace3b4a64b1fb31528db0518a
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-06-30 14:35:07 +1000

    double-serial-list added

--- utl/list.go
+++ utl/list.go
@@ -4,7 +4,9 @@
 
 package utl
 
-// DblList implements a matrix with variable number of entities
+import "github.com/cpmech/gosl/io"
+
+// DblList implements a tabular list with variable number of columns
 //  Example:
 //    Vals = [][]float64{
 //             {0.0},
@@ -29,4 +31,47 @@ func (o *DblList) Append(rowidx int, value float64) {
 
 	// append value
 	o.Vals[rowidx] = append(o.Vals[rowidx], value)
+}
+
+// DblSlist implements a tabular list with variable number of columns
+// using a serial representation
+//  Example:
+//      0.0
+//      1.0  1.1  1.2  1.3
+//      2.0  2.1
+//      3.0  3.1  3.2
+//  becomes:
+//             (0)   (1)    2    3    4   (5)    6   (7)    8    9   (10)
+//      Vals = 0.0 | 1.0  1.1  1.2  1.3 | 2.0  2.1 | 3.0  3.1  3.2 |
+//      Ptrs = 0 1 5 7 10
+//  Notes:
+//      len(Ptrs) = nrows + 1
+//      Ptrs[len(Ptrs)-1] = len(Vals)
+type DblSlist struct {
+	Vals []float64 // values
+	Ptrs []int     // pointers
+}
+
+// Append appends item to DblSlist
+func (o *DblSlist) Append(startRow bool, value float64) {
+	if startRow {
+		if len(o.Ptrs) == 0 {
+			o.Ptrs = []int{0, 0}
+		} else {
+			start := o.Ptrs[len(o.Ptrs)-1]
+			o.Ptrs = append(o.Ptrs, start)
+		}
+	}
+	o.Vals = append(o.Vals, value)
+	o.Ptrs[len(o.Ptrs)-1] += 1
+}
+
+// Print prints the souble-serial-list
+func (o DblSlist) Print(fmt string) {
+	for i := 0; i < len(o.Ptrs)-1; i++ {
+		for j := o.Ptrs[i]; j < o.Ptrs[i+1]; j++ {
+			io.Pf(fmt, o.Vals[j])
+		}
+		io.Pf("\n")
+	}
 }
--- utl/t_list_test.go
+++ utl/t_list_test.go
@@ -33,3 +33,84 @@ func Test_list01(tst *testing.T) {
 		chk.PrintFail("DblList Append")
 	}
 }
+
+func Test_list02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("list02. DblSlist.Append")
+
+	var L DblSlist
+
+	L.Append(true, 0.0)
+	io.Pforan("L.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "0: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "0: vals", 1e-17, L.Vals, []float64{0.0})
+	chk.Ints(tst, "0: ptrs", L.Ptrs, []int{0, 1})
+
+	L.Append(true, 1.0)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "1: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "1: vals", 1e-17, L.Vals, []float64{0.0, 1.0})
+	chk.Ints(tst, "1: ptrs", L.Ptrs, []int{0, 1, 2})
+
+	L.Append(false, 1.1)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "2: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "2: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1})
+	chk.Ints(tst, "2: ptrs", L.Ptrs, []int{0, 1, 3})
+
+	L.Append(false, 1.2)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "3: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "3: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2})
+	chk.Ints(tst, "3: ptrs", L.Ptrs, []int{0, 1, 4})
+
+	L.Append(false, 1.3)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "4: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "4: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3})
+	chk.Ints(tst, "4: ptrs", L.Ptrs, []int{0, 1, 5})
+
+	L.Append(true, 2.0)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "5: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "5: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3, 2.0})
+	chk.Ints(tst, "5: ptrs", L.Ptrs, []int{0, 1, 5, 6})
+
+	L.Append(false, 2.1)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "6: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "6: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3, 2.0, 2.1})
+	chk.Ints(tst, "6: ptrs", L.Ptrs, []int{0, 1, 5, 7})
+
+	L.Append(true, 3.0)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "7: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "7: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3, 2.0, 2.1, 3.0})
+	chk.Ints(tst, "7: ptrs", L.Ptrs, []int{0, 1, 5, 7, 8})
+
+	L.Append(false, 3.1)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "8: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "8: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3, 2.0, 2.1, 3.0, 3.1})
+	chk.Ints(tst, "8: ptrs", L.Ptrs, []int{0, 1, 5, 7, 9})
+
+	L.Append(false, 3.2)
+	io.Pforan("\nL.Vals = %v\n", L.Vals)
+	io.Pfpink("L.Ptrs = %v\n", L.Ptrs)
+	chk.Ints(tst, "9: lens", []int{L.Ptrs[len(L.Ptrs)-1]}, []int{len(L.Vals)})
+	chk.Vector(tst, "9: vals", 1e-17, L.Vals, []float64{0.0, 1.0, 1.1, 1.2, 1.3, 2.0, 2.1, 3.0, 3.1, 3.2})
+	chk.Ints(tst, "9: ptrs", L.Ptrs, []int{0, 1, 5, 7, 10})
+
+	io.Pf("\n")
+	L.Print("%10g")
+}
