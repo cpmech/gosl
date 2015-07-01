22bb4777792e6b4fd9f69c0ab3d33c1fb1cb08ed
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-07-02 00:44:42 +1000

    bidimensional integration using trapezoidal and simpsons rule added

--- num/integration.go
+++ num/integration.go
@@ -43,6 +43,150 @@ func TrapzRange(xa, xb float64, npts int, y Cb_yx) (A float64) {
 	return A
 }
 
+// Trapz2D computes a double integral over the x-y plane; thus resulting on
+// the volume defined between the function f(x,y) and the plane ortogonal to z
+func Trapz2D(dx, dy float64, f [][]float64) (V float64) {
+
+	// check
+	if len(f) < 2 {
+		chk.Panic("len(f)=%d is incorrect; it must be at least 2", len(f))
+	}
+	m, n := len(f), len(f[0])
+
+	// corners
+	V = f[0][0] + f[m-1][0] + f[0][n-1] + f[m-1][n-1]
+	//M := utl.IntsAlloc(m, n)
+	//M[0][0] = 1
+	//M[m-1][0] = 1
+	//M[0][n-1] = 1
+	//M[m-1][n-1] = 1
+
+	// top/bottom: 2
+	for j := 1; j < n-1; j++ {
+		V += 2.0 * (f[0][j] + f[m-1][j])
+		//M[0][j] = 2
+		//M[m-1][j] = 2
+	}
+
+	// left/right: 2
+	for i := 1; i < m-1; i++ {
+		V += 2.0 * (f[i][0] + f[i][n-1])
+		//M[i][0] = 2
+		//M[i][n-1] = 2
+	}
+
+	// centre: 4
+	for i := 1; i < m-1; i++ {
+		for j := 1; j < n-1; j++ {
+			V += 4.0 * f[i][j]
+			//M[i][j] = 4
+		}
+	}
+
+	// final result
+	V *= dx * dy / 4.0
+
+	// debug
+	//for i := 0; i < m; i++ {
+	//for j := 0; j < n; j++ {
+	//io.Pf("%4d", M[i][j])
+	//}
+	//io.Pf("\n")
+	//}
+	return
+}
+
+// Simps2D computes a double integral over the x-y plane (Simpson's rule); thus resulting on
+// the volume defined between the function f(x,y) and the plane ortogonal to z
+func Simps2D(dx, dy float64, f [][]float64) (V float64) {
+
+	// check
+	if len(f) < 2 {
+		chk.Panic("len(f)=%d is incorrect; it must be at least 2", len(f))
+	}
+	m, n := len(f), len(f[0])
+
+	// corners
+	V = f[0][0] + f[m-1][0] + f[0][n-1] + f[m-1][n-1]
+	//M := utl.IntsAlloc(m, n)
+	//M[0][0] = 1
+	//M[m-1][0] = 1
+	//M[0][n-1] = 1
+	//M[m-1][n-1] = 1
+
+	// top/bottom: 4
+	for j := 1; j < n-1; j += 2 {
+		V += 4.0 * (f[0][j] + f[m-1][j])
+		//M[0][j] = 4
+		//M[m-1][j] = 4
+	}
+
+	// top/bottom: 2
+	for j := 2; j < n-1; j += 2 {
+		V += 2.0 * (f[0][j] + f[m-1][j])
+		//M[0][j] = 2
+		//M[m-1][j] = 2
+	}
+
+	// left/right: 4
+	for i := 1; i < m-1; i += 2 {
+		V += 4.0 * (f[i][0] + f[i][n-1])
+		//M[i][0] = 4
+		//M[i][n-1] = 4
+	}
+
+	// left/right: 2
+	for i := 2; i < m-1; i += 2 {
+		V += 2.0 * (f[i][0] + f[i][n-1])
+		//M[i][0] = 2
+		//M[i][n-1] = 2
+	}
+
+	// centre: 4
+	for i := 2; i < m-1; i += 2 {
+		for j := 2; j < n-1; j += 2 {
+			V += 4.0 * f[i][j]
+			//M[i][j] = 4
+		}
+	}
+
+	// centre: 8a
+	for i := 1; i < m-1; i += 2 {
+		for j := 2; j < n-1; j += 2 {
+			V += 8.0 * f[i][j]
+			//M[i][j] = 8
+		}
+	}
+
+	// centre: 8b
+	for i := 2; i < m-1; i += 2 {
+		for j := 1; j < n-1; j += 2 {
+			V += 8.0 * f[i][j]
+			//M[i][j] = 8
+		}
+	}
+
+	// centre: 16
+	for i := 1; i < m-1; i += 2 {
+		for j := 1; j < n-1; j += 2 {
+			V += 16.0 * f[i][j]
+			//M[i][j] = 16
+		}
+	}
+
+	// final result
+	V *= dx * dy / 9.0
+
+	// debug
+	//for i := 0; i < m; i++ {
+	//for j := 0; j < n; j++ {
+	//io.Pf("%4d", M[i][j])
+	//}
+	//io.Pf("\n")
+	//}
+	return
+}
+
 // error messages
 var (
 	_trapz_err1 = "trapz.go: Trapz: length of x and y must be the same. %d != %d"
--- num/t_integ_test.go
+++ num/t_integ_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/cpmech/gosl/chk"
 	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/la"
 	"github.com/cpmech/gosl/utl"
 )
 
@@ -42,4 +43,102 @@ func Test_trapz02(tst *testing.T) {
 	Acor := 1.08306090851465 // right value is Acor := 1.08268158558
 	chk.Scalar(tst, "A", 1e-15, A, Acor)
 	chk.Scalar(tst, "A_", 1e-15, A_, Acor)
+}
+
+func Test_2dinteg01(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("2dinteg01. volume of box")
+
+	x := utl.LinSpace(-1, 1, 7)
+	y := utl.LinSpace(-2, 2, 9)
+	m, n := len(x), len(y)
+	f := la.MatAlloc(m, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			f[i][j] = 3.0
+		}
+	}
+	dx, dy := x[1]-x[0], y[1]-y[0]
+	Vt := Trapz2D(dx, dy, f)
+	Vs := Simps2D(dx, dy, f)
+	io.Pforan("Vt = %v\n", Vt)
+	io.Pforan("Vs = %v\n", Vs)
+	chk.Scalar(tst, "Vt", 1e-14, Vt, 24.0)
+	chk.Scalar(tst, "Vs", 1e-14, Vs, 24.0)
+}
+
+func Test_2dinteg02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("2dinteg02. bidimensional integral")
+
+	// Γ(1/4, 1)
+	gamma_1div4_1 := 0.2462555291934987088744974330686081384629028737277219
+
+	x := utl.LinSpace(0, 1, 11)
+	y := utl.LinSpace(0, 1, 11)
+	m, n := len(x), len(y)
+	f := la.MatAlloc(m, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			f[i][j] = 8.0 * math.Exp(-math.Pow(x[i], 2)-math.Pow(y[j], 4))
+		}
+	}
+	dx, dy := x[1]-x[0], y[1]-y[0]
+	Vt := Trapz2D(dx, dy, f)
+	Vs := Simps2D(dx, dy, f)
+	Vc := math.Sqrt(math.Pi) * math.Erf(1) * (math.Gamma(1.0/4.0) - gamma_1div4_1)
+	io.Pforan("Vt = %v\n", Vt)
+	io.Pforan("Vs = %v\n", Vs)
+	io.Pfgreen("Vc = %v\n", Vc)
+	chk.Scalar(tst, "Vt", 0.0114830435645548, Vt, Vc)
+	chk.Scalar(tst, "Vs", 1e-4, Vs, Vc)
+
+}
+
+func Test_2dinteg03(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("2dinteg03. ∫∫(1+8xy)dxdy")
+
+	x := utl.LinSpace(0, 3, 5)
+	y := utl.LinSpace(1, 2, 5)
+	m, n := len(x), len(y)
+	f := la.MatAlloc(m, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			f[i][j] = 1.0 + 8.0*x[i]*y[j]
+		}
+	}
+	dx, dy := x[1]-x[0], y[1]-y[0]
+	Vt := Trapz2D(dx, dy, f)
+	Vs := Simps2D(dx, dy, f)
+	io.Pforan("Vt = %v\n", Vt)
+	io.Pforan("Vs = %v\n", Vs)
+	chk.Scalar(tst, "Vt", 1e-13, Vt, 57.0)
+	chk.Scalar(tst, "Vs", 1e-13, Vs, 57.0)
+}
+
+func Test_2dinteg04(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("2dinteg04. ∫∫y・sin(x)")
+
+	x := utl.LinSpace(0, math.Pi/2.0, 11)
+	y := utl.LinSpace(0, 1, 11)
+	m, n := len(x), len(y)
+	f := la.MatAlloc(m, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			f[i][j] = math.Sin(x[i]) * y[j]
+		}
+	}
+	dx, dy := x[1]-x[0], y[1]-y[0]
+	Vt := Trapz2D(dx, dy, f)
+	Vs := Simps2D(dx, dy, f)
+	io.Pforan("Vt = %v\n", Vt)
+	io.Pforan("Vs = %v\n", Vs)
+	chk.Scalar(tst, "Vt", 0.00103, Vt, 0.5)
+	chk.Scalar(tst, "Vs", 1e-5, Vs, 0.5)
 }
