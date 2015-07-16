5b12bb3f79d830e1495a471da25ecf326b0868d3
Dorival Pedroso <pedroso@cpmech.com>
2015-07-16 14:24:27 +1000

    dist point added to bezier but it's not ready yet

--- gm/bezier.go
+++ gm/bezier.go
@@ -5,8 +5,12 @@
 package gm
 
 import (
+	"math"
+
 	"github.com/cpmech/gosl/chk"
 	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/num"
+	"github.com/cpmech/gosl/plt"
 )
 
 // BezierQuad implements a quadratic Bezier curve
@@ -15,7 +19,12 @@ import (
 //  A = Q2 - 2 Q1 + Q0
 //  B = Q1 - Q0
 type BezierQuad struct {
+
+	// input
 	Q [][]float64 // control points; can be set outside
+
+	// auxiliary
+	P []float64 // a point on curve
 }
 
 // Point returns the x-y-z coordinates of a point on Bezier curve
@@ -32,12 +41,21 @@ func (o *BezierQuad) Point(C []float64, t float64) {
 }
 
 // DistPoint returns the distance from a point to this Bezier curve
-func (o *BezierQuad) DistPoint(X []float64) float64 {
+// It finds the closest projection which is stored in P
+func (o *BezierQuad) DistPoint(X []float64, doplot bool) float64 {
+
+	// TODO:
+	//   1) split this into closest projections finding
+	//   2) finish distance computation
+
+	// check
 	if len(o.Q) != 3 {
 		chk.Panic("DistPoint: quadratic Bezier must be initialised first (with 3 control points)")
 	}
 	ndim := len(o.Q[0])
 	chk.IntAssert(len(X), ndim)
+
+	// solve cubic equation
 	var A_i, B_i, M_i, a, b, c, d float64
 	for i := 0; i < ndim; i++ {
 		A_i = o.Q[2][i] - 2.0*o.Q[1][i] + o.Q[0][i]
@@ -48,6 +66,54 @@ func (o *BezierQuad) DistPoint(X []float64) float64 {
 		c += 2.0*B_i*B_i + M_i*A_i
 		d += M_i * B_i
 	}
-	io.Pforan("a=%v b=%v c=%v d=%v\n", a, b, c, d)
+	//io.Pforan("a=%v b=%v c=%v d=%v\n", a, b, c, d)
+	if math.Abs(a) < 1e-7 {
+		chk.Panic("DistPoint does not yet work with this type of Bezier (straight line?):\nQ=%v\n", o.Q)
+	}
+	x1, x2, x3, nx := num.EqCubicSolveReal(b/a, c/a, d/a)
+	io.Pfyel("\nx1=%v x2=%v x3=%v nx=%v\n", x1, x2, x3, nx)
+
+	// auxiliary
+	if len(o.P) != ndim {
+		o.P = make([]float64, ndim)
+	}
+
+	// closest projections
+	t := x1
+	if nx == 2 {
+		chk.Panic("nx=2 => not implemented yet")
+	}
+	if nx == 3 {
+		T := []float64{x1, x2, x3}
+		D := []float64{-1, -1, -1}
+		ok := []bool{
+			!(x1 < 0.0 || x1 > 1.0),
+			!(x2 < 0.0 || x2 > 1.0),
+			!(x3 < 0.0 || x3 > 1.0),
+		}
+		io.Pforan("ok = %v\n", ok)
+		for i, t := range T {
+			if ok[i] {
+				o.Point(o.P, t)
+				if doplot {
+					plt.PlotOne(X[0], X[1], "'ko'")
+					plt.PlotOne(o.P[0], o.P[1], "'k.'")
+					plt.Arrow(X[0], X[1], o.P[0], o.P[1], "ec='none'")
+				}
+				D[i] = ppdist(X, o.P)
+			}
+		}
+		io.Pforan("D = %v\n", D)
+	}
+	o.Point(o.P, t)
+	io.Pfcyan("P = %v\n", o.P)
 	return 0
+}
+
+// ppdist computes point-point distance
+func ppdist(a, b []float64) (d float64) {
+	for i := 0; i < len(a); i++ {
+		d += (a[i] - b[i]) * (a[i] - b[i])
+	}
+	return math.Sqrt(d)
 }
--- gm/t_bezier_test.go
+++ gm/t_bezier_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/io"
 	"github.com/cpmech/gosl/plt"
 	"github.com/cpmech/gosl/utl"
 )
@@ -15,7 +16,7 @@ import (
 func Test_bezier01(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("bezier01. quadratic Bezier. point-distance")
+	chk.PrintTitle("bezier01. quadratic Bezier.")
 
 	bez := BezierQuad{
 		Q: [][]float64{
@@ -24,8 +25,6 @@ func Test_bezier01(tst *testing.T) {
 			{2, 4},
 		},
 	}
-
-	doplot := false
 
 	np := 21
 	T := utl.LinSpace(0, 1, np)
@@ -41,12 +40,63 @@ func Test_bezier01(tst *testing.T) {
 		Y2[i] = X2[i] * X2[i]
 		chk.Scalar(tst, "y=y", 1e-15, Y[i], X[i]*X[i])
 	}
-	if doplot {
+
+	if false {
 		plt.SetForPng(1, 400, 200)
 		plt.Plot(X2, Y2, "'y-', lw=4,label='y=x*x'")
 		plt.Plot(X, Y, "'b-', marker='.', label='Bezier'")
 		plt.Gll("x", "y", "")
 		plt.Equal()
 		plt.SaveD("/tmp", "fig_gm_bezier01.png")
+	}
+}
+
+func Test_bezier02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("bezier02. quadratic Bezier. point-distance")
+
+	bez := BezierQuad{
+		Q: [][]float64{
+			{-1, 1},
+			{0.5, -2},
+			{2, 4},
+		},
+	}
+
+	nx, ny := 5, 5
+	xx, yy := utl.MeshGrid2D(-1.5, 2.5, -0.5, 4.5, nx, ny)
+	//zz := la.MatAlloc(nx, ny)
+
+	// TODO: finish this test
+
+	doplot := false
+	if doplot {
+		plt.SetForPng(1, 400, 200)
+	}
+
+	C := make([]float64, 2)
+	for j := 0; j < ny; j++ {
+		for i := 0; i < nx; i++ {
+			C[0], C[1] = xx[i][j], yy[i][j]
+			d := bez.DistPoint(C, doplot)
+			io.Pforan("d = %v\n", d)
+		}
+	}
+
+	np := 21
+	T := utl.LinSpace(0, 1, np)
+	X := make([]float64, np)
+	Y := make([]float64, np)
+	for i, t := range T {
+		bez.Point(C, t)
+		X[i], Y[i] = C[0], C[1]
+	}
+
+	if doplot {
+		plt.Plot(X, Y, "'b-', label='Bezier'")
+		plt.Gll("x", "y", "")
+		plt.Equal()
+		plt.SaveD("/tmp", "fig_gm_bezier02.png")
 	}
 }
