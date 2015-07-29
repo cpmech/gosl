37d6204905386825b8441e9dd541b9cdd173b350
Dorival Pedroso <pedroso@cpmech.com>
2015-07-29 12:32:23 +1000

    stat table added

--- rnd/stat.go
+++ rnd/stat.go
@@ -8,16 +8,19 @@ import (
 	"math"
 
 	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/la"
 )
 
 // Stat performs some basic statistics
 //  Input:
-//   x -- sample
+//   x   -- sample
+//   std -- compute standard deviation (σ) instead of average deviation (adev)
 //  Output:
-//   mean -- mean average (first moment)
-//   adev -- average deviation
-//   sdev -- standrad deviation
-func Stat(x []float64) (mean, adev, sdev float64) {
+//   xmin -- minimum value
+//   xave -- mean average (first moment)
+//   xmax -- maximum value
+//   xdev -- average deviation; if std==true, computes standard deviation (σ) instead
+func Stat(x []float64, std bool) (xmin, xave, xmax, xdev float64) {
 
 	// check
 	n := len(x)
@@ -25,28 +28,38 @@ func Stat(x []float64) (mean, adev, sdev float64) {
 		return
 	}
 
-	// first pass to get the mean
+	// average, min and max
 	var sum float64
+	xmin, xmax = x[0], x[0]
 	for i := 0; i < n; i++ {
 		sum += x[i]
+		xmin = min(xmin, x[i])
+		xmax = max(xmax, x[i])
 	}
 	N := float64(n)
-	mean = sum / N
+	xave = sum / N
 
-	// second pass to get the deviations
-	var d, c, p, vari float64
-	for i := 0; i < n; i++ {
-		d = x[i] - mean     // d ← xi - bar(x)
-		adev += math.Abs(d) // adev ← Σ |d|
-		c += d              // c ← Σ d  // corrector
-		p = d * d           // p ← d²
-		vari += p           // vari ← Σ d²
+	// standard deviation
+	if std {
+		var d, c, vari float64
+		for i := 0; i < n; i++ {
+			d = x[i] - xave // d ← xi - bar(x)
+			c += d          // c ← Σ d  (corrector)
+			vari += d * d   // vari ← Σ d²
+		}
+		vari = (vari - c*c/N) / (N - 1.0)
+		xdev = math.Sqrt(vari)
+		return
 	}
 
-	// put the pieces together according to the conventional definitions
-	adev /= N
-	vari = (vari - c*c/N) / (N - 1.0)
-	sdev = math.Sqrt(vari)
+	// average deviation
+	sum = 0
+	var d float64
+	for i := 0; i < n; i++ {
+		d = x[i] - xave    // d ← xi - bar(x)
+		sum += math.Abs(d) // sum ← Σ |d|
+	}
+	xdev = sum / N
 	return
 }
 
@@ -104,5 +117,55 @@ func Moments(x []float64) (sum, mean, adev, sdev, vari, skew, kurt float64, err 
 	}
 	skew /= (N * vari * sdev)
 	kurt = kurt/(N*vari*vari) - 3.0
+	return
+}
+
+// StatTable computes the min, ave, max, and dev of values organised in a table
+//  Input:
+//   x     -- sample
+//   std   -- compute standard deviation (σ) instead of average deviation (adev)
+//   withZ -- computes z-matrix as well
+//  Convention of indices:
+//   0=min  1=ave  2=max  3=dev
+//  Output:                        min          ave          max          dev
+//                                  ↓            ↓            ↓            ↓
+//   x00 x01 x02 x03 x04 x05 → y00=min(x0?) y10=ave(x0?) y20=max(x0?) y30=dev(x0?)
+//   x10 x11 x12 x13 x14 x15 → y01=min(x1?) y11=ave(x1?) y21=max(x1?) y31=dev(x1?)
+//   x20 x21 x22 x23 x24 x25 → y02=min(x2?) y12=ave(x2?) y22=max(x2?) y32=dev(x2?)
+//                                  ↓            ↓            ↓            ↓
+//                       min → z00=min(y0?) z01=min(y1?) z02=min(y2?) z03=min(y3?)
+//                       ave → z10=ave(y0?) z11=ave(y1?) z12=ave(y2?) z13=ave(y3?)
+//                       max → z20=max(y0?) z21=max(y1?) z22=max(y2?) z23=max(y3?)
+//                       dev → z30=dev(y0?) z31=dev(y1?) z32=dev(y2?) z33=dev(y3?)
+//                                  =            =            =            =
+//                       min → z00=min(min) z01=min(ave) z02=min(max) z03=min(dev)
+//                       ave → z10=ave(min) z11=ave(ave) z12=ave(max) z13=ave(dev)
+//                       max → z20=max(min) z21=max(ave) z22=max(max) z23=max(dev)
+//                       dev → z30=dev(min) z31=dev(ave) z32=dev(max) z33=dev(dev)
+func StatTable(x [][]float64, std, withZ bool) (y, z [][]float64) {
+
+	// dimensions
+	m := len(x)
+	if m < 1 {
+		return
+	}
+	n := len(x[0])
+	if n < 2 {
+		return
+	}
+
+	// compute y
+	y = la.MatAlloc(4, m)
+	for i := 0; i < m; i++ {
+		y[0][i], y[1][i], y[2][i], y[3][i] = Stat(x[i], std)
+	}
+
+	// compute z
+	if withZ {
+		z = la.MatAlloc(4, 4)
+		for i := 0; i < 4; i++ {
+			z[0][i], z[1][i], z[2][i], z[3][i] = Stat(y[i], std)
+		}
+	}
 	return
 }
--- rnd/t_stat_test.go
+++ rnd/t_stat_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/cpmech/gosl/chk"
 	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/la"
 )
 
 func Test_stat01(tst *testing.T) {
@@ -18,7 +19,8 @@ func Test_stat01(tst *testing.T) {
 
 	x := []float64{100, 100, 102, 98, 77, 99, 70, 105, 98}
 
-	mean1, adev1, sdev1 := Stat(x)
+	xmin, xave, xmax, xdevS := Stat(x, true)
+	_, _, _, xdevA := Stat(x, false)
 
 	sum, mean, adev, sdev, vari, skew, kurt, err := Moments(x)
 	if err != nil {
@@ -26,20 +28,73 @@ func Test_stat01(tst *testing.T) {
 	}
 	io.Pforan("x    = %v\n", x)
 	io.Pforan("sum  = %v\n", sum)
-	io.Pforan("mean = %v  (%v)\n", mean, mean1)
-	io.Pforan("adev = %v  (%v)\n", adev, adev1)
-	io.Pforan("sdev = %v  (%v)\n", sdev, sdev1)
+	io.Pforan("mean = %v  (%v)\n", mean, xave)
+	io.Pforan("adev = %v  (%v)\n", adev, xdevA)
+	io.Pforan("sdev = %v  (%v)\n", sdev, xdevS)
 	io.Pforan("vari = %v\n", vari)
 	io.Pforan("skew = %v\n", skew)
 	io.Pforan("kurt = %v\n", kurt)
-	chk.Scalar(tst, "sum ", 1e-17, sum, 849)
-	chk.Scalar(tst, "mean", 1e-17, mean, 849.0/9.0)
-	chk.Scalar(tst, "sdev", 1e-17, sdev, 12.134661099511597)
-	chk.Scalar(tst, "vari", 1e-17, vari, 147.25)
-
-	chk.Scalar(tst, "adev1", 1e-17, adev1, adev)
-	chk.Scalar(tst, "mean1", 1e-17, mean1, 849.0/9.0)
-	chk.Scalar(tst, "sdev1", 1e-17, sdev1, 12.134661099511597)
-
+	chk.Scalar(tst, "sum  ", 1e-17, sum, 849)
+	chk.Scalar(tst, "mean ", 1e-17, mean, 849.0/9.0)
+	chk.Scalar(tst, "sdev ", 1e-17, sdev, 12.134661099511597)
+	chk.Scalar(tst, "vari ", 1e-17, vari, 147.25)
+	chk.Scalar(tst, "xmin ", 1e-17, xmin, 70)
+	chk.Scalar(tst, "xave ", 1e-17, xave, 849.0/9.0)
+	chk.Scalar(tst, "xmax ", 1e-17, xmax, 105)
+	chk.Scalar(tst, "xdevA", 1e-17, xdevA, adev)
+	chk.Scalar(tst, "xdevS", 1e-17, xdevS, 12.134661099511597)
 	// TODO: add checks for adev, skew and kurt
+}
+
+func Test_stat02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("stat02")
+
+	x := [][]float64{
+		{100, 100, 102, 98, 77, 99, 70, 105, 98},
+		{80, 101, 12, 58, 47, 80, 20, 111, 89},
+		{50, 130, 72, 38, 71, 15, 10, 12, 55},
+	}
+
+	y, z := StatTable(x, true, true)
+	la.PrintMat("x", x, "%5g", false)
+	la.PrintMat("y", y, "%13.6f", false)
+	la.PrintMat("z", z, "%13.6f", false)
+	io.Pforan("\nmin\n")
+	chk.Scalar(tst, "y00=min(x[0,:])", 1e-17, y[0][0], 70)
+	chk.Scalar(tst, "y01=min(x[1,:])", 1e-17, y[0][1], 12)
+	chk.Scalar(tst, "y02=min(x[2,:])", 1e-17, y[0][2], 10)
+	io.Pforan("\nave\n")
+	chk.Scalar(tst, "y10=ave(x[0,:])", 1e-17, y[1][0], 849.0/9.0)
+	chk.Scalar(tst, "y11=ave(x[1,:])", 1e-17, y[1][1], 598.0/9.0)
+	chk.Scalar(tst, "y12=ave(x[2,:])", 1e-17, y[1][2], 453.0/9.0)
+	io.Pforan("\nmax\n")
+	chk.Scalar(tst, "y20=max(x[0,:])", 1e-17, y[2][0], 105)
+	chk.Scalar(tst, "y21=max(x[1,:])", 1e-17, y[2][1], 111)
+	chk.Scalar(tst, "y22=max(x[2,:])", 1e-17, y[2][2], 130)
+	io.Pforan("\ndev\n")
+	chk.Scalar(tst, "y30=dev(x[0,:])", 1e-17, y[3][0], 12.134661099511597)
+	chk.Scalar(tst, "y31=dev(x[1,:])", 1e-17, y[3][1], 34.688294535444918)
+	chk.Scalar(tst, "y32=dev(x[2,:])", 1e-17, y[3][2], 38.343839140075687)
+	io.Pfyel("\nmin\n")
+	chk.Scalar(tst, "z00=min(y[0,:])=min(min)", 1e-17, z[0][0], 10)
+	chk.Scalar(tst, "z01=min(y[1,:])=min(ave)", 1e-17, z[0][1], 453.0/9.0)
+	chk.Scalar(tst, "z02=min(y[2,:])=min(max)", 1e-17, z[0][2], 105)
+	chk.Scalar(tst, "z03=min(y[3,:])=min(dev)", 1e-17, z[0][3], 12.134661099511597)
+	io.Pfyel("\nave\n")
+	chk.Scalar(tst, "z10=ave(y[0,:])=ave(min)", 1e-17, z[1][0], 92.0/3.0)
+	chk.Scalar(tst, "z11=ave(y[1,:])=ave(ave)", 1e-17, z[1][1], ((849.0+598.0+453.0)/9.0)/3.0)
+	chk.Scalar(tst, "z12=ave(y[2,:])=ave(max)", 1e-17, z[1][2], 346.0/3.0)
+	chk.Scalar(tst, "z13=ave(y[3,:])=ave(dev)", 1e-17, z[1][3], (12.134661099511597+34.688294535444918+38.343839140075687)/3.0)
+	io.Pfyel("\nmax\n")
+	chk.Scalar(tst, "z20=max(y[0,:])=max(min)", 1e-17, z[2][0], 70)
+	chk.Scalar(tst, "z21=max(y[1,:])=max(ave)", 1e-17, z[2][1], 849.0/9.0)
+	chk.Scalar(tst, "z22=max(y[2,:])=max(max)", 1e-17, z[2][2], 130)
+	chk.Scalar(tst, "z23=max(y[3,:])=max(dev)", 1e-17, z[2][3], 38.343839140075687)
+	io.Pfyel("\ndev\n")
+	chk.Scalar(tst, "z30=dev(y[0,:])=dev(min)", 1e-17, z[3][0], 34.078341117685483)
+	chk.Scalar(tst, "z31=dev(y[1,:])=dev(ave)", 1e-17, z[3][1], 22.261169573539771)
+	chk.Scalar(tst, "z32=dev(y[2,:])=dev(max)", 1e-17, z[3][2], 13.051181300301263)
+	chk.Scalar(tst, "z33=dev(y[3,:])=dev(dev)", 1e-17, z[3][3], 14.194778389023206)
 }
