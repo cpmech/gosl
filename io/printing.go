ce5472ab13b37ae8c37ace848ce606dce71bc0ef
Dorival Pedroso <pedroso@cpmech.com>
2015-08-09 12:30:09 +1000

    printing.go added: str thick and thin lines and spaces added to io

---
+++ io/printing.go
@@ -0,0 +1,29 @@
+// Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package io
+
+// StrThickLine returns a thick line (using '=')
+func StrThickLine(n int) (l string) {
+	for i := 0; i < n; i++ {
+		l += "="
+	}
+	return l + "\n"
+}
+
+// StrThinLine returns a thin line (using '-')
+func StrThinLine(n int) (l string) {
+	for i := 0; i < n; i++ {
+		l += "-"
+	}
+	return l + "\n"
+}
+
+// StrSpaces returns a line with spaces
+func StrSpaces(n int) (l string) {
+	for i := 0; i < n; i++ {
+		l += " "
+	}
+	return
+}
