ead18656d459a2c15b80371801f2b2ac2b106805
Dorival Pedroso <pedroso@cpmech.com>
2015-08-14 22:39:43 +1000

    tool to read LP fortran file converted to function

--- opt/readproblem.go
+++ opt/readproblem.go
@@ -2,20 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build ignore
-
-package main
+package opt
 
 import (
 	"strings"
 
 	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/la"
 )
 
-func main() {
-
-	// filename
-	fn, _ := io.ArgToFilename(0, "afiro", ".dat", true)
+// ReadLPfortran reads linear program from particular fortran file
+//  download LP files from here: http://users.clas.ufl.edu/hager/coap/format.html
+//  Output:
+//   A -- compressed-column sparse matrix where:
+//        Ap -- pointers to the begining of storage of column (size n+1)
+//        Ai -- row indices for each non zero entry (input, nnz A)
+//        Ax -- non zero entries (input, nnz A)
+//   b -- right hand side (input, size m)
+//   c -- objective vector (minimize, size n)
+//   l -- lower bounds on variables (size n)
+//   u -- upper bounds on variables (size n)
+func ReadLPfortran(fn string) (A *la.CCMatrix, b, c, l, u []float64) {
 
 	// variables
 	var m int        // number or rows (input)
@@ -23,11 +30,7 @@ func main() {
 	var Ap []int     // pointers to the begining of storage of column (size n+1)
 	var Ai []int     // row indices for each non zero entry (input, nnz A)
 	var Ax []float64 // non zero entries (input, nnz A)
-	var b []float64  // right hand side (input, size m)
-	var c []float64  // objective vector (minimize, size n)
 	var z0 float64   // initial fixed value for objective
-	var l []float64  // lower bounds on variables (size n)
-	var u []float64  // upper bounds on variables (size n)
 
 	// auxiliary
 	reading_Ap := false
@@ -138,11 +141,19 @@ func main() {
 		return
 	})
 
-	io.Pforan("Ap = %v\n", Ap)
-	io.Pfcyan("Ai = %v\n", Ai)
-	io.Pfyel("Ax = %v\n", Ax)
-	io.Pf("b = %v\n", b)
-	io.Pforan("c = %v\n", c)
-	io.Pfcyan("l = %v\n", l)
-	io.Pfyel("u = %v\n", u)
+	// debug
+	if false {
+		io.Pforan("Ap = %v\n", Ap)
+		io.Pfcyan("Ai = %v\n", Ai)
+		io.Pfyel("Ax = %v\n", Ax)
+		io.Pf("b = %v\n", b)
+		io.Pforan("c = %v\n", c)
+		io.Pfcyan("l = %v\n", l)
+		io.Pfyel("u = %v\n", u)
+	}
+
+	// results
+	A = new(la.CCMatrix)
+	A.Set(m, n, Ap, Ai, Ax)
+	return
 }
