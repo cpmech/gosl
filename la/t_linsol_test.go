0918e18c444011a4a74f7d635fa73106e37cce25
Dorival Pedroso <pedroso@cpmech.com>
2015-09-08 14:51:30 +1000

    test with complex matrix added

--- la/t_linsol_test.go
+++ la/t_linsol_test.go
@@ -92,9 +92,9 @@ func run_linsol_testC(tst *testing.T, t *TripletC, tol_cmp, tol_res float64, b, 
 	A := t.ToMatrix(nil)
 	if verbose {
 		io.Pforan("A.x = b\n")
-		PrintMatC("A", A.ToDense(), "(%g+", "%gi) ", false)
-		PrintVecC("x", x, "(%g+", "%gi) ", false)
-		PrintVecC("b", b, "(%g+", "%gi) ", false)
+		PrintMatC("A", A.ToDense(), "(%g", "%+gi) ", false)
+		PrintVecC("x", x, "(%g", "%+gi) ", false)
+		PrintVecC("b", b, "(%g", "%+gi) ", false)
 	}
 
 	// check
@@ -281,6 +281,93 @@ func Test_linsol05(tst *testing.T) {
 			ar*imag(x_correct[i])+ac*real(x_correct[i]))
 	}
 
-	// run text
+	// run test
 	run_linsol_testC(tst, &t, 1e-14, 1e-13, b, x_correct, true)
+}
+
+func Test_linsol06(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("linsol06. complex (with complex components)")
+
+	// given the following matrix of complex numbers:
+	//      _                                                  _
+	//     |  19.73    12.11-i      5i        0          0      |
+	//     |  -0.51i   32.3+7i    23.07       i          0      |
+	// A = |    0      -0.51i    70+7.3i     3.95    19+31.83i  |
+	//     |    0        0        1+1.1i    50.17      45.51    |
+	//     |_   0        0          0      -9.351i       55    _|
+	//
+	// and the following vector:
+	//      _                  _
+	//     |    77.38+8.82i     |
+	//     |   157.48+19.8i     |
+	// b = |  1175.62+20.69i    |
+	//     |   912.12-801.75i   |
+	//     |_     550-1060.4i  _|
+	//
+	// solve:
+	//         A.x = b
+	//
+	// the solution is:
+	//      _            _
+	//     |     3.3-i    |
+	//     |    1+0.17i   |
+	// x = |      5.5     |
+	//     |       9      |
+	//     |_  10-17.75i _|
+
+	// flag indicating to store (real,complex) values in monolithic form => 1D array
+	xzmono := false
+
+	// input matrix in Complex Triplet format
+	var t TripletC
+	t.Init(5, 5, 16, xzmono) // 5 x 5 matrix with 16 non-zeros
+
+	// first column
+	t.Put(0, 0, 19.73, 0) // i=0, j=0, real=19.73, complex=0
+	t.Put(1, 0, 0, -0.51) // i=1, j=0, real=0, complex=-0.51
+
+	// second column
+	t.Put(0, 1, 12.11, -1) // i=0, j=1, real=12.11, complex=-1
+	t.Put(1, 1, 32.3, 7)
+	t.Put(2, 1, 0, -0.51)
+
+	// third column
+	t.Put(0, 2, 0, 5)
+	t.Put(1, 2, 23.07, 0)
+	t.Put(2, 2, 70, 7.3)
+	t.Put(3, 2, 1, 1.1)
+
+	// fourth column
+	t.Put(1, 3, 0, 1)
+	t.Put(2, 3, 3.95, 0)
+	t.Put(3, 3, 50.17, 0)
+	t.Put(4, 3, 0, -9.351)
+
+	// fifth column
+	t.Put(2, 4, 19, 31.83)
+	t.Put(3, 4, 45.51, 0)
+	t.Put(4, 4, 55, 0)
+
+	// right-hand-side
+	b := []complex128{
+		77.38 + 8.82i,
+		157.48 + 19.8i,
+		1175.62 + 20.69i,
+		912.12 - 801.75i,
+		550 - 1060.4i,
+	}
+
+	// solution
+	x_correct := []complex128{
+		3.3 - 1i,
+		1 + 0.17i,
+		5.5,
+		9,
+		10 - 17.75i,
+	}
+
+	// run text
+	run_linsol_testC(tst, &t, 1e-3, 1e-12, b, x_correct, true)
 }
