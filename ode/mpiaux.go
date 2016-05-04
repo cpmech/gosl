7722487bba9838b16d9ad3bec920416b485dc958
Dorival Pedroso <pedroso@cpmech.com>
2016-05-04 20:18:40 +1000

    aux mpi function for radau5 mpi added

--- ode/mpiaux.go
+++ ode/mpiaux.go
@@ -6,5 +6,9 @@
 
 package ode
 
-func (o *Ode) init_mpi() {
+func (o *ODE) init_mpi() {
+}
+
+func radau5_step_mpi(o *ODE, y0 []float64, x0 float64, args ...interface{}) (rerr float64, err error) {
+return
 }
