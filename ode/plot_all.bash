effe1a52a129d11f03d9cf09a959b1f17d7d2118
Dorival Pedroso <pedroso@cpmech.com>
2016-05-04 21:39:36 +1000

    unused files removed. test file renamed

--- ode/plot_all.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in results/*.py; do
-    python $f
-done
-
-for f in data/*.py; do
-    python $f
-done
--- ode/xrunmpitests.bash
+++ ode/xrunmpitests.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go build -o /tmp/gosl/t_ODE01b_main t_ODE01b_main.go && mpirun -np 2 /tmp/gosl/t_ODE01b_main
-go build -o /tmp/gosl/t_ODE04b_main t_ODE04b_main.go && mpirun -np 3 /tmp/gosl/t_ODE04b_main
+go build -o /tmp/gosl/t_ode01_main t_ode01_main.go && mpirun -np 2 /tmp/gosl/t_ode01_main
+go build -o /tmp/gosl/t_ode04_main t_ode04_main.go && mpirun -np 3 /tmp/gosl/t_ode04_main
