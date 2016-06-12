07b02c541df795b6a05554ebb47e1a760f4ac55e
Dorival Pedroso <pedroso@cpmech.com>
2016-06-12 15:17:15 +1000

    test cleaned

--- gm/msh/monitor.bash
+++ gm/msh/monitor.bash
@@ -6,5 +6,5 @@ while true; do
     inotifywait -q -e modify $FILES
     echo
     echo
-    go test -test.run="mesh01"
+    go test -test.run="singleq4"
 done
--- gm/msh/t_mesh_test.go
+++ gm/msh/t_mesh_test.go
@@ -5,7 +5,6 @@
 package msh
 
 import (
-	"sort"
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
@@ -27,176 +26,49 @@ func Test_singleq4(tst *testing.T) {
 	// correct data
 	nverts := 4
 	ncells := 1
-	X := [][]float64{
+	allX := [][]float64{
 		{0, 0},
 		{1, 0},
 		{1, 1},
 		{0, 1},
 	}
-	vtags := []int{-1, 0, -1, 0}
-	ctags := []int{-1}
-	parts := []int{0}
-	types := []string{"qua4"}
-	V := [][]int{
+	allVtags := []int{-1, 0, -1, 0}
+	allCtags := []int{-1}
+	allParts := []int{0}
+	allTypes := []string{"qua4"}
+	allV := [][]int{
 		{0, 1, 2, 3},
 	}
-	etags := [][]int{
+	allEtags := [][]int{
 		{-10, -11, -12, -13},
 	}
 
 	// check input data
-	checkinput(tst, m, nverts, ncells, X, vtags, ctags, parts, types, V, etags, nil)
+	checkinput(tst, m, nverts, ncells, allX, allVtags, allCtags, allParts, allTypes, allV, allEtags, nil)
 
 	// get map of tags
-	d, err := m.GetTagMaps()
+	tm, err := m.GetTagMaps()
 	if err != nil {
 		tst.Errorf("GetTagMaps failed:\n%v\n", err)
 		return
 	}
 
-	// check VertTag2verts
-	chk.IntAssert(len(d.VertTag2verts), 1)
-	var neg1verts []int
-	io.Pfyel("\nVertTag2verts:\n")
-	for key, val := range d.VertTag2verts {
-		io.Pf("%v => %v\n", key, val)
-		for _, v := range val {
-			if v.Tag == -1 {
-				neg1verts = append(neg1verts, v.Id)
-			}
-		}
-	}
-	sort.Ints(neg1verts)
-	chk.Ints(tst, "-1 vertices", neg1verts, []int{0, 2})
+	// correct data
+	vtags := []int{-1}
+	ctags := []int{-1}
+	cparts := []int{0}
+	etags := []int{-10, -11, -12, -13}
+	ctypes := []string{"qua4"}
+	vtagsVids := [][]int{{0, 2}}
+	ctagsCids := [][]int{{0}}
+	cpartsCids := [][]int{{0}}
+	ctypesCids := [][]int{{0}}
+	etagsCids := [][]int{{0}, {0}, {0}, {0}} // not unique
+	etagsLocEids := [][]int{{0}, {1}, {2}, {3}}
+	etagsVids := [][]int{{0, 1}, {1, 2}, {2, 3}, {0, 3}}
 
-	// check CellTag2cells
-	chk.IntAssert(len(d.CellTag2cells), 1)
-	var neg1cells []int
-	io.Pfyel("\nCellTag2cells:\n")
-	for key, val := range d.CellTag2cells {
-		io.Pf("%v => %v\n", key, val)
-		for _, c := range val {
-			if c.Tag == -1 {
-				neg1cells = append(neg1cells, c.Id)
-			}
-		}
-	}
-	sort.Ints(neg1cells)
-	chk.Ints(tst, "-1 cells", neg1cells, []int{0})
-
-	// check CellType2cells
-	chk.IntAssert(len(d.CellType2cells), 1)
-	var qua4cells []int
-	io.Pfyel("\nCellType2cells:\n")
-	for key, val := range d.CellType2cells {
-		io.Pf("%v => %v\n", key, val)
-		for _, c := range val {
-			if c.Type == "qua4" {
-				qua4cells = append(qua4cells, c.Id)
-			}
-		}
-	}
-	sort.Ints(qua4cells)
-	chk.Ints(tst, "qua4 cells", qua4cells, []int{0})
-
-	// check CellPart2cells
-	chk.IntAssert(len(d.CellPart2cells), 1)
-	var part0cells []int
-	io.Pfyel("\nCellPart2cells:\n")
-	for key, val := range d.CellPart2cells {
-		io.Pf("%v => %v\n", key, val)
-		for _, c := range val {
-			if c.Part == 0 {
-				part0cells = append(part0cells, c.Id)
-			}
-		}
-	}
-	sort.Ints(part0cells)
-	chk.Ints(tst, "part 0 cells", part0cells, []int{0})
-
-	// check EdgeTag2cells
-	chk.IntAssert(len(d.EdgeTag2cells), 4)
-	var neg10edgeLids []int
-	var neg10edgeCells []int
-	var neg11edgeLids []int
-	var neg11edgeCells []int
-	var neg12edgeLids []int
-	var neg12edgeCells []int
-	var neg13edgeLids []int
-	var neg13edgeCells []int
-	io.Pfyel("\nEdgeTag2cells:\n")
-	for key, val := range d.EdgeTag2cells {
-		io.Pf("%v => %v\n", key, val)
-		for _, pair := range val {
-			id := pair.BryId
-			c := pair.C
-			if c.EdgeTags[id] == -10 {
-				neg10edgeLids = append(neg10edgeLids, id)
-				neg10edgeCells = append(neg10edgeCells, c.Id)
-			}
-			if c.EdgeTags[id] == -11 {
-				neg11edgeLids = append(neg11edgeLids, id)
-				neg11edgeCells = append(neg11edgeCells, c.Id)
-			}
-			if c.EdgeTags[id] == -12 {
-				neg12edgeLids = append(neg12edgeLids, id)
-				neg12edgeCells = append(neg12edgeCells, c.Id)
-			}
-			if c.EdgeTags[id] == -13 {
-				neg13edgeLids = append(neg13edgeLids, id)
-				neg13edgeCells = append(neg13edgeCells, c.Id)
-			}
-		}
-	}
-	sort.Ints(neg10edgeLids)
-	sort.Ints(neg10edgeCells)
-	sort.Ints(neg11edgeLids)
-	sort.Ints(neg11edgeCells)
-	sort.Ints(neg12edgeLids)
-	sort.Ints(neg12edgeCells)
-	sort.Ints(neg13edgeLids)
-	sort.Ints(neg13edgeCells)
-	chk.Ints(tst, "-10 edge => lids ", neg10edgeLids, []int{0})
-	chk.Ints(tst, "-10 edge => cells", neg10edgeCells, []int{0})
-	chk.Ints(tst, "-11 edge => lids ", neg11edgeLids, []int{1})
-	chk.Ints(tst, "-11 edge => cells", neg11edgeCells, []int{0})
-	chk.Ints(tst, "-12 edge => lids ", neg12edgeLids, []int{2})
-	chk.Ints(tst, "-12 edge => cells", neg12edgeCells, []int{0})
-	chk.Ints(tst, "-13 edge => lids ", neg13edgeLids, []int{3})
-	chk.Ints(tst, "-13 edge => cells", neg13edgeCells, []int{0})
-
-	// check EdgeTag2verts
-	chk.IntAssert(len(d.EdgeTag2verts), 4)
-	var neg10edgeVerts []int
-	var neg11edgeVerts []int
-	var neg12edgeVerts []int
-	var neg13edgeVerts []int
-	io.Pfyel("\nEdgeTag2verts:\n")
-	for key, val := range d.EdgeTag2verts {
-		io.Pf("%v => %v\n", key, val)
-		for _, v := range val {
-			if key == -10 {
-				neg10edgeVerts = append(neg10edgeVerts, v.Id)
-			}
-			if key == -11 {
-				neg11edgeVerts = append(neg11edgeVerts, v.Id)
-			}
-			if key == -12 {
-				neg12edgeVerts = append(neg12edgeVerts, v.Id)
-			}
-			if key == -13 {
-				neg13edgeVerts = append(neg13edgeVerts, v.Id)
-			}
-		}
-	}
-	sort.Ints(neg10edgeVerts)
-	sort.Ints(neg11edgeVerts)
-	sort.Ints(neg12edgeVerts)
-	sort.Ints(neg13edgeVerts)
-	chk.Ints(tst, "-10 edge => verts", neg10edgeVerts, []int{0, 1})
-	chk.Ints(tst, "-11 edge => verts", neg11edgeVerts, []int{1, 2})
-	chk.Ints(tst, "-12 edge => verts", neg12edgeVerts, []int{2, 3})
-	chk.Ints(tst, "-13 edge => verts", neg13edgeVerts, []int{0, 3})
+	// check maps
+	checkmaps(tst, m, tm, vtags, ctags, cparts, etags, ctypes, vtagsVids, ctagsCids, cpartsCids, ctypesCids, etagsVids, etagsCids, etagsLocEids)
 }
 
 func Test_mesh01(tst *testing.T) {
@@ -268,9 +140,9 @@ func Test_mesh01(tst *testing.T) {
 	ctagsCids := [][]int{{0, 1, 2}, {3, 4, 5}}
 	cpartsCids := [][]int{{0, 3}, {1, 4}, {2, 5}}
 	ctypesCids := [][]int{{0, 1, 3, 4}, {2, 5}}
-	etagsVids := [][]int{{0, 1, 2}, {2, 3, 6, 7, 10}, {8, 9, 10}, {0, 4, 8}}
 	etagsCids := [][]int{{0, 1}, {2, 2, 5, 5}, {3, 4}, {0, 3}} // not unique
 	etagsLocEids := [][]int{{0, 0}, {0, 1, 0, 1}, {2, 2}, {3, 3}}
+	etagsVids := [][]int{{0, 1, 2}, {2, 3, 6, 7, 10}, {8, 9, 10}, {0, 4, 8}}
 
 	// check maps
 	checkmaps(tst, m, tm, vtags, ctags, cparts, etags, ctypes, vtagsVids, ctagsCids, cpartsCids, ctypesCids, etagsVids, etagsCids, etagsLocEids)
