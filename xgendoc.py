7d713d09dfb663220f337ac42126674852212bef
Dorival Pedroso <pedroso@cpmech.com>
2016-06-29 18:37:43 +1000

    message fixed

--- xgendoc.py
+++ xgendoc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
+# Copyright 2016 The Gosl Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
