c6fccbaf2f61bd896ed5cfc0dd53d39508bb8b61
Dorival Pedroso <pedroso@cpmech.com>
2016-06-29 19:06:41 +1000

    doc updated

--- gm/plotbsplines.go
+++ gm/plotbsplines.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// gm (geometry and meshes) implements auxiliary functions for
-// handling geometry and mesh structures
 package gm
 
 import (
