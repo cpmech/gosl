4aa3ff7331ce74481c71bf1325f00e3f0db46f3b
Dorival Pedroso <pedroso@cpmech.com>
2016-08-14 19:08:07 +1000

    Implement setting of env vars on Windows Installer

--- doc/install/create-gosl-install.bash
+++ doc/install/create-gosl-install.bash
@@ -6,7 +6,7 @@
 # add these two to the PATH
 # C:\Program Files (x86)\WiX Toolset v3.10\bin
 
-heat.exe dir SourceDir -gg -sfrag -sreg -srd -cg GoslComp -out goslfrags.wxs
-candle.exe goslfrags.wxs goslapp.wxs
-light.exe -ext WixUIExtension -out gosl-installer.msi goslfrags.wixobj goslapp.wixobj
-rm *.wixobj
+heat.exe dir SourceDir -gg -sfrag -sreg -srd -dr GOSLDIR -cg GoslComp -out goslfrags.wxs
+candle.exe goslfrags.wxs goslapp.wxs varsdlg.wxs myui.wxs
+light.exe -ext WixUIExtension -ext WixUtilExtension -out gosl-installer.msi goslfrags.wixobj goslapp.wixobj varsdlg.wixobj myui.wixobj
+rm *.wixobj *.wixpdb
