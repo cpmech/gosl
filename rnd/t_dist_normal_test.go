53f29c65bf56359b541acb7321be93a82123fedc
Dorival Pedroso <pedroso@cpmech.com>
2017-04-12 14:19:31 +1000

    Add test for normal distribution

--- rnd/t_dist_normal_test.go
+++ rnd/t_dist_normal_test.go
@@ -5,6 +5,7 @@
 package rnd
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
@@ -13,13 +14,13 @@ import (
 	"github.com/cpmech/gosl/utl"
 )
 
-func plot_normal(μ, σ float64) {
+func plot_normal(μ, σ, xmin, xmax float64) {
 
 	var dist DistNormal
 	dist.Init(&VarData{M: μ, S: σ})
 
 	n := 101
-	x := utl.LinSpace(-2, 2, n)
+	x := utl.LinSpace(xmin, xmax, n)
 	y := make([]float64, n)
 	Y := make([]float64, n)
 	for i := 0; i < n; i++ {
@@ -100,7 +101,7 @@ func Test_dist_normal_02(tst *testing.T) {
 	if doplot {
 		plt.SetForEps(1.5, 300)
 		for _, σ := range []float64{1, 0.5, 0.25} {
-			plot_normal(0, σ)
+			plot_normal(0, σ, -2, 2)
 		}
 		plt.SaveD("/tmp/gosl", "rnd_dist_normal_02.eps")
 	}
@@ -144,5 +145,52 @@ func Test_dist_normal_04(tst *testing.T) {
 		plt.Plot(x, y, "'b-'")
 		plt.Gll("$x$", "$\\Phi^{-1}(\\Phi(x))$", "")
 		plt.SaveD("/tmp/gosl", "rnd_dist_normal_04.eps")
+	}
+}
+
+func Test_dist_normal_05(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("dist_normal_05. random numbers")
+
+	μ := 1.0
+	σ := 0.25
+
+	nsamples := 10000
+	X := make([]float64, nsamples)
+	for i := 0; i < nsamples; i++ {
+		X[i] = μ + σ*rand.NormFloat64()
+	}
+
+	nstations := 41
+	xmin := 0.0
+	xmax := 3.0
+	dx := (xmax - xmin) / float64(nstations-1)
+
+	var hist Histogram
+	hist.Stations = utl.LinSpace(xmin, xmax, nstations)
+	hist.Count(X, true)
+
+	prob := make([]float64, nstations)
+	for i := 0; i < nstations-1; i++ {
+		prob[i] = float64(hist.Counts[i]) / (float64(nsamples) * dx)
+	}
+
+	io.Pf(TextHist(hist.GenLabels("%.3f"), hist.Counts, 60))
+	io.Pforan("dx = %v\n", dx)
+
+	area := 0.0
+	for i := 0; i < nstations-1; i++ {
+		area += dx * prob[i]
+	}
+	io.Pforan("area = %v\n", area)
+	chk.Scalar(tst, "area", 1e-15, area, 1)
+
+	if chk.Verbose {
+		plt.SetForEps(1.5, 300)
+		plot_normal(μ, σ, 0, 2)
+		plt.Subplot(2, 1, 1)
+		hist.PlotDensity(nil, "")
+		plt.SaveD("/tmp/gosl", "rnd_dist_normal_05.eps")
 	}
 }
