b0b317f2d6333d56a229a6dca37bd9c741e4a265
Dorival Pedroso <pedroso@cpmech.com>
2017-05-29 11:24:35 +1000

    Organise comments in num.deriv

--- num/deriv.go
+++ num/deriv.go
@@ -11,21 +11,21 @@ type Cb_fx func(x float64) float64
 // DerivCen5 approximates the derivative of f w.r.t x using central differences with 5 points.
 func DerivCen5(f Cb_fx, x, h float64) (res, abserr float64) {
 
+	// first estimate
 	res, round, trunc := centralDeriv5(f, x, h)
 	err := round + trunc
 
+	// second estimate
 	if round < trunc && (round > 0 && trunc > 0) {
 
-		/* Compute an optimised stepsize to minimize the total error, using the scaling of the truncation
-		   error (O(h^2)) and rounding error (O(1/h)). */
-
+		// Compute an optimised stepsize to minimize the total error,
+		// using the scaling of the truncation error (O(h^2)) and rounding error (O(1/h)).
 		h_opt := h * math.Pow(round/(2.0*trunc), 1.0/3.0)
 		r_opt, round_opt, trunc_opt := centralDeriv5(f, x, h_opt)
 		error_opt := round_opt + trunc_opt
 
-		/* Check that the new error is smaller, and that the new derivative
-		   is consistent with the error bounds of the original estimate. */
-
+		// Check that the new error is smaller, and that the new derivative
+		// is consistent with the error bounds of the original estimate.
 		if error_opt < err && math.Abs(r_opt-res) < 4.0*err {
 			res = r_opt
 			err = error_opt
@@ -39,21 +39,21 @@ func DerivCen5(f Cb_fx, x, h float64) (res, abserr float64) {
 // DerivFwd4 approximates the derivative of f w.r.t x using forward differences with 4 points.
 func DerivFwd4(f Cb_fx, x, h float64) (res, abserr float64) {
 
+	// first estimate
 	res, round, trunc := forwardDeriv4(f, x, h)
 	err := round + trunc
 
+	// second estimate
 	if round < trunc && (round > 0 && trunc > 0) {
 
-		/* Compute an optimised stepsize to minimize the total error, using the scaling of the estimated
-		   truncation error (O(h)) and rounding error (O(1/h)). */
-
+		// Compute an optimised stepsize to minimize the total error,
+		// using the scaling of the estimated truncation error (O(h)) and rounding error (O(1/h)).
 		h_opt := h * math.Pow(round/(trunc), 1.0/2.0)
 		r_opt, round_opt, trunc_opt := forwardDeriv4(f, x, h_opt)
 		error_opt := round_opt + trunc_opt
 
-		/* Check that the new error is smaller, and that the new derivative
-		   is consistent with the error bounds of the original estimate. */
-
+		// Check that the new error is smaller, and that the new derivative
+		// is consistent with the error bounds of the original estimate.
 		if error_opt < err && math.Abs(r_opt-res) < 4.0*err {
 			res = r_opt
 			err = error_opt
@@ -64,20 +64,20 @@ func DerivFwd4(f Cb_fx, x, h float64) (res, abserr float64) {
 	return
 }
 
-func DerivBackward(f Cb_fx, x, h float64) (res, abserr float64) {
+// DerivBwd4 approximates the derivative of f w.r.t x using backward differences with 4 points.
+func DerivBwd4(f Cb_fx, x, h float64) (res, abserr float64) {
 	return DerivFwd4(f, x, -h)
 }
 
 // lower level functions //////////////////////////////////////////////////////////////////////////
 
+// centralDeriv5 computes the derivative using the 5-point rule (x-h, x-h/2, x, x+h/2, x+h).
 func centralDeriv5(f Cb_fx, x float64, h float64) (res, absErrRound, absErrTrunc float64) {
 
-	/* Compute the derivative using the 5-point rule (x-h, x-h/2, x, x+h/2, x+h).
-	   Note that the central point is not used.
-	   Compute the error using the difference between the 5-point and the 3-point rule (x-h,x,x+h).
-	   Again the central point is not used.
-	*/
-
+	// Compute the derivative using the 5-point rule (x-h, x-h/2, x, x+h/2, x+h).
+	// Note that the central point is not used.
+	// Compute the error using the difference between the 5-point and the 3-point rule (x-h,x,x+h).
+	// Again the central point is not used.
 	fm1 := f(x - h)
 	fp1 := f(x + h)
 	fmh := f(x - h/2)
@@ -87,26 +87,23 @@ func centralDeriv5(f Cb_fx, x float64, h float64) (res, absErrRound, absErrTrunc
 	e3 := (math.Abs(fp1) + math.Abs(fm1)) * DBL_EPSILON
 	e5 := 2.0*(math.Abs(fph)+math.Abs(fmh))*DBL_EPSILON + e3
 
-	/* The next term is due to finite precision in x+h = O (eps * x) */
-
+	// The next term is due to finite precision in x+h = O (eps * x)
 	dy := max(math.Abs(r3/h), math.Abs(r5/h)) * (math.Abs(x) / h) * DBL_EPSILON
 
-	/* The truncation error in the r5 approximation itself is O(h^4).
-	   However, for safety, we estimate the error from r5-r3, which is O(h^2).
-	   By scaling h we will minimise this estimated error, not the actual truncation error in r5. */
-
+	// The truncation error in the r5 approximation itself is O(h^4).
+	// However, for safety, we estimate the error from r5-r3, which is O(h^2).
+	// By scaling h we will minimise this estimated error, not the actual truncation error in r5.
 	res = r5 / h
 	absErrTrunc = math.Abs((r5 - r3) / h) // Estimated truncation error O(h^2)
 	absErrRound = math.Abs(e5/h) + dy     // Rounding error (cancellations)
-
 	return
 }
 
+// forwardDeriv4 compute the derivative using the 4-point rule (x+h/4, x+h/2, x+3h/4, x+h).
 func forwardDeriv4(f Cb_fx, x, h float64) (res, absErrRound, absErrTrunc float64) {
 
-	/* Compute the derivative using the 4-point rule (x+h/4, x+h/2, x+3h/4, x+h).
-	   Compute the error using the difference between the 4-point and the 2-point rule (x+h/2,x+h). */
-
+	// Compute the derivative using the 4-point rule (x+h/4, x+h/2, x+3h/4, x+h).
+	// Compute the error using the difference between the 4-point and the 2-point rule (x+h/2,x+h).
 	f1 := f(x + h/4.0)
 	f2 := f(x + h/2.0)
 	f3 := f(x + (3.0/4.0)*h)
@@ -114,18 +111,15 @@ func forwardDeriv4(f Cb_fx, x, h float64) (res, absErrRound, absErrTrunc float64
 	r2 := 2.0 * (f4 - f2)
 	r4 := (22.0/3.0)*(f4-f3) - (62.0/3.0)*(f3-f2) + (52.0/3.0)*(f2-f1)
 
-	/* Estimate the rounding error for r4 */
-
+	// Estimate the rounding error for r4
 	e4 := 2 * 20.67 * (math.Abs(f4) + math.Abs(f3) + math.Abs(f2) + math.Abs(f1)) * DBL_EPSILON
 
-	/* The next term is due to finite precision in x+h = O (eps * x) */
-
+	// The next term is due to finite precision in x+h = O (eps * x)
 	dy := max(math.Abs(r2/h), math.Abs(r4/h)) * math.Abs(x/h) * DBL_EPSILON
 
-	/* The truncation error in the r4 approximation itself is O(h^3). However, for safety, we estimate the
-	   error from r4-r2, which is O(h). By scaling h we will minimise this estimated error, not
-	   the actual truncation error in r4. */
-
+	// The truncation error in the r4 approximation itself is O(h^3).
+	// However, for safety, we estimate the error from r4-r2, which is O(h).
+	// By scaling h we will minimise this estimated error, not the actual truncation error in r4.
 	res = r4 / h
 	absErrTrunc = math.Abs((r4 - r2) / h) // Estimated truncation error O(h)
 	absErrRound = math.Abs(e4/h) + dy
