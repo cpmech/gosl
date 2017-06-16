326daf4154e4632d5cbd32060b5cc4e505a9c772
Dorival Pedroso <pedroso@cpmech.com>
2017-06-16 15:23:16 +1000

    Improve lagrange interpolator and tests. Add Runge phenomenon test

--- fun/lagrange.go
+++ fun/lagrange.go
@@ -4,34 +4,138 @@
 
 package fun
 
-import "github.com/cpmech/gosl/chk"
+import (
+	"math"
 
-// LagrangeCardinal computes the [i] Lagrange cardinal p-degree polynomial @ x
+	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/utl"
+)
+
+// enums
+var (
+
+	// UniformGridKind defines the uniform 1D grid kind
+	UniformGridKind = io.NewEnum("Uniform", "fun.uniform", "U", "Uniform 1D grid")
+)
+
+// LagrangeInterp implements Lagrange interpolators associated with a grid X
 //
-//              p
-//     p       ━━━━      x  - X[j]
-//    L  (x) = ┃  ┃  ——————————————
-//     i       ┃  ┃    X[i] - X[j]
-//            j = 0
-//            j ≠ i
+//   An interpolant I^X_N{f} (associated with a grid X; of degree N; with N+1 points)
+//   is expressed in the Lagrange form as follows:
+//
+//                     N
+//         X          ————             X
+//        I {f}(x) =  \     f(x[i]) ⋅ ℓ (x)
+//         N          /                i
+//                    ————
+//                    i = 0
+//
+//   where ℓ^X_i(x) is the i-th Lagrange cardinal polynomial associated with grid X and given by:
+//
+//                 N
+//         N      ━━━━    x  -  X[j]
+//        ℓ (x) = ┃  ┃  —————————————           0 ≤ i ≤ N
+//         i      ┃  ┃   X[i] - X[j]
+//               j = 0
+//               j ≠ i
+//
+type LagrangeInterp struct {
+	N int       // degree: N = len(X)-1
+	X []float64 // grid points: len(X) = P+1; generated in [-1, 1]
+}
+
+// NewLagrangeInterp allocates a new LagrangeInterp
+//   N        -- degree
+//   gridType -- type of grid; e.g. uniform
+//   NOTE: the grid will be generated in [-1, 1]
+func NewLagrangeInterp(N int, gridType io.Enum) (o *LagrangeInterp, err error) {
+	o = new(LagrangeInterp)
+	o.N = N
+	switch gridType {
+	case UniformGridKind:
+		o.X = utl.LinSpace(-1, 1, N+1)
+	default:
+		return nil, chk.Err("cannot create grid type %q\n", gridType)
+	}
+	return
+}
+
+// L computes the i-th Lagrange cardinl polynomial ℓ^X_i(x) associated with grid X
+//
+//                 N
+//         X      ━━━━    x  -  X[j]
+//        ℓ (x) = ┃  ┃  —————————————           0 ≤ i ≤ N
+//         i      ┃  ┃   X[i] - X[j]
+//               j = 0
+//               j ≠ i
 //
 //   Input:
-//      p -- degree
 //      i -- index of X[i] point
-//      x -- where to evaluate the L^p_i polynomial
-//      X -- the p+1 points
+//      x -- where to evaluate the polynomial
 //   Output:
-//      lpi -- L^p_i(x)
-//
-func LagrangeCardinal(p, i int, x float64, X []float64) (lpi float64) {
-	if len(X) != p+1 {
-		chk.Panic("len(X) must be equal to p+1")
-	}
-	lpi = 1
-	for j := 0; j < p+1; j++ {
+//      lix -- ℓ^X_i(x)
+func (o *LagrangeInterp) L(i int, x float64) (lix float64) {
+	lix = 1
+	for j := 0; j < o.N+1; j++ {
 		if i != j {
-			lpi *= (x - X[j]) / (X[i] - X[j])
+			lix *= (x - o.X[j]) / (o.X[i] - o.X[j])
 		}
+	}
+	return
+}
+
+// I computes the interpolation I^X_N{f}(x) @ x
+//
+//                     N
+//         X          ————             X
+//        I {f}(x) =  \     f(x[i]) ⋅ ℓ (x)
+//         N          /                i
+//                    ————
+//                    i = 0
+//
+func (o *LagrangeInterp) I(x float64, f Ss) (ix float64, err error) {
+	for i := 0; i < o.N+1; i++ {
+		fxi, e := f(o.X[i])
+		if e != nil {
+			return 0, e
+		}
+		ix += fxi * o.L(i, x)
+	}
+	return
+}
+
+// Lebesgue estimates the Lebesgue constant by using 10000 stations along [-1,1]
+func (o *LagrangeInterp) Lebesgue() (ΛN float64) {
+	nsta := 10000 // generate several points along [-1,1]
+	for j := 0; j < nsta; j++ {
+		x := -1.0 + 2.0*float64(j)/float64(nsta-1)
+		sum := math.Abs(o.L(0, x))
+		for i := 1; i < o.N+1; i++ {
+			sum += math.Abs(o.L(i, x))
+		}
+		if sum > ΛN {
+			ΛN = sum
+		}
+	}
+	return
+}
+
+// MaxErr estimates the maximum error using 10000 stations along [-1,1]
+func (o *LagrangeInterp) MaxErr(f Ss) (maxerr float64) {
+	nsta := 10000 // generate several points along [-1,1]
+	for i := 0; i < nsta; i++ {
+		x := -1.0 + 2.0*float64(i)/float64(nsta-1)
+		fx, err := f(x)
+		if err != nil {
+			chk.Panic("f(x) failed:%v\n", err)
+		}
+		ix, err := o.I(x, f)
+		if err != nil {
+			chk.Panic("I(x) failed:%v\n", err)
+		}
+		e := math.Abs(fx - ix)
+		maxerr = max(maxerr, e)
 	}
 	return
 }
--- fun/t_lagrange_test.go
+++ fun/t_lagrange_test.go
@@ -5,6 +5,7 @@
 package fun
 
 import (
+	"math"
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
@@ -18,36 +19,145 @@ func TestLagCardinal01(tst *testing.T) {
 	//verbose()
 	chk.PrintTitle("LagCardinal01. Lagrange cardinal polynomials")
 
-	xa, xb := -1.0, 1.0
-	p := 4
-	X := utl.LinSpace(xa, xb, p+1)
-	for i := 0; i < p+1; i++ {
-		for j, x := range X {
-			lpi := LagrangeCardinal(p, i, x, X)
+	N := 5
+	o, err := NewLagrangeInterp(N, UniformGridKind)
+	if err != nil {
+		tst.Errorf("%v\n", err)
+		return
+	}
+	chk.Scalar(tst, "Λ (Lebesgue constant)", 1e-15, o.Lebesgue(), 3.106301040275436e+00)
+
+	for i := 0; i < N+1; i++ {
+		for j, x := range o.X {
+			li := o.L(i, x)
 			ana := 1.0
 			if i != j {
 				ana = 0
 			}
-			chk.AnaNum(tst, io.Sf("L^%d_%d(X[%d])", p, i, j), 1e-17, lpi, ana, chk.Verbose)
+			chk.AnaNum(tst, io.Sf("L^%d_%d(X[%d])", N, i, j), 1e-17, li, ana, chk.Verbose)
 		}
 		io.Pl()
 	}
 
 	if chk.Verbose {
-		xx := utl.LinSpace(xa, xb, 201)
+		xx := utl.LinSpace(-1, 1, 201)
 		yy := make([]float64, len(xx))
 		plt.Reset(true, nil)
-		for n := 0; n < p+1; n++ {
+		for n := 0; n < N+1; n++ {
 			for k, x := range xx {
-				yy[k] = LagrangeCardinal(p, n, x, X)
+				yy[k] = o.L(n, x)
 			}
 			plt.Plot(xx, yy, &plt.A{NoClip: true})
 		}
-		Y := make([]float64, len(X))
-		plt.Plot(X, Y, &plt.A{C: "k", Ls: "none", M: "o", Void: true, NoClip: true})
+		Y := make([]float64, N+1)
+		plt.Plot(o.X, Y, &plt.A{C: "k", Ls: "none", M: "o", Void: true, NoClip: true})
 		plt.Gll("x", "y", nil)
 		plt.Cross(0, 0, &plt.A{C: "grey"})
 		plt.HideAllBorders()
 		plt.Save("/tmp/gosl/fun", "lagcardinal01")
+	}
+}
+
+func TestLagInterp01(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("TestLagInterp01. Lagrange interpolation")
+
+	f := func(x float64) (float64, error) {
+		return math.Cos(math.Exp(2.0 * x)), nil
+	}
+
+	N := 5
+	o, err := NewLagrangeInterp(N, UniformGridKind)
+	if err != nil {
+		tst.Errorf("%v\n", err)
+		return
+	}
+
+	for i, x := range o.X {
+		ynum, err := o.I(x, f)
+		if err != nil {
+			tst.Errorf("%v\n", err)
+			return
+		}
+		yana, _ := f(x)
+		chk.AnaNum(tst, io.Sf("I(X[%d])", i), 1e-17, ynum, yana, chk.Verbose)
+	}
+	io.Pl()
+
+	if chk.Verbose {
+		xx := utl.LinSpace(-1, 1, 201)
+		yy := make([]float64, len(xx))
+		for k, x := range xx {
+			yy[k], _ = f(x)
+		}
+		iy := make([]float64, len(xx))
+		plt.Reset(true, nil)
+		plt.Plot(xx, yy, &plt.A{C: "k", Lw: 4, NoClip: true})
+		for _, N := range []int{4, 6, 8, 12, 16, 24} {
+			p, _ := NewLagrangeInterp(N, UniformGridKind)
+			for k, x := range xx {
+				iy[k], _ = p.I(x, f)
+			}
+			E := p.MaxErr(f)
+			plt.Plot(xx, iy, &plt.A{L: io.Sf("$N=%2d\\;E=%.3e$", N, E), NoClip: true})
+		}
+		plt.Gll("x", "y", nil)
+		plt.Cross(0, 0, &plt.A{C: "grey"})
+		plt.HideAllBorders()
+		plt.Save("/tmp/gosl/fun", "laginterp01")
+	}
+}
+
+func TestLagInterp02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("TestLagInterp02. Lagrange interpolation")
+
+	f := func(x float64) (float64, error) {
+		return 1.0 / (1.0 + 16.0*x*x), nil
+	}
+
+	N := 5
+	o, err := NewLagrangeInterp(N, UniformGridKind)
+	if err != nil {
+		tst.Errorf("%v\n", err)
+		return
+	}
+
+	for i, x := range o.X {
+		ynum, err := o.I(x, f)
+		if err != nil {
+			tst.Errorf("%v\n", err)
+			return
+		}
+		yana, _ := f(x)
+		chk.AnaNum(tst, io.Sf("I(X[%d])", i), 1e-17, ynum, yana, chk.Verbose)
+	}
+	io.Pl()
+
+	if chk.Verbose {
+		xx := utl.LinSpace(-1, 1, 201)
+		yy := make([]float64, len(xx))
+		for k, x := range xx {
+			yy[k], _ = f(x)
+		}
+		iy := make([]float64, len(xx))
+		plt.Reset(true, nil)
+		plt.Plot(xx, yy, &plt.A{C: "k", Lw: 4, NoClip: true})
+		for _, N := range []int{4, 6, 8, 12, 16, 24} {
+			p, _ := NewLagrangeInterp(N, UniformGridKind)
+			for k, x := range xx {
+				iy[k], _ = p.I(x, f)
+			}
+			E := p.MaxErr(f)
+			plt.Plot(xx, iy, &plt.A{L: io.Sf("$N=%2d\\;E=%.3e$", N, E), NoClip: true})
+		}
+		plt.Gll("x", "y", nil)
+		plt.Cross(0, 0, &plt.A{C: "grey"})
+		plt.AxisYmax(1)
+		plt.AxisYmin(-1)
+		plt.HideAllBorders()
+		plt.Save("/tmp/gosl/fun", "laginterp02")
 	}
 }
