88de5cf426d63767981cdaa3e77a761f174e8c1f
Dorival Pedroso <pedroso@cpmech.com>
2017-06-17 19:08:09 +1000

    Change flag to compile fftw on darwin by default

--- fun/fftw/fftw.go
+++ fun/fftw/fftw.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !windows,!darwin
+// +build !windows
 
 // Package fftw wraps the FFTW library to perform Fourier Transforms
 // using the "fast" method by Cooley and Tukey
