cc6322683b0f98f666f0bef130fab4e7c45e07a6
Dorival Pedroso <pedroso@cpmech.com>
2017-06-20 17:38:19 +1000

    Add auxiliary python file

---
+++ la/oblas/data/auxiliary.py
@@ -0,0 +1,22 @@
+# Copyright 2016 The Gosl Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+def vprint(name, v):
+    l = '%s := []float64{' % name
+    for i in range(len(v)):
+        if i > 0: l += ','
+        l += '%23.15e' % v[i]
+    l += '}'
+    print l
+
+def mprint(name, m):
+    l = '%s := [][]float64{\n' % name
+    for i in range(len(m)):
+        l += '    {'
+        for j in range(len(m[i])):
+            if j > 0: l += ','
+            l += '%23.15e' % m[i][j]
+        l += '},\n'
+    l += '}'
+    print l
