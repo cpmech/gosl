51562b8aacd9904f4980a15f22f223c9da0b9a57
Dorival Pedroso <pedroso@cpmech.com>
2017-06-23 15:52:21 +1000

    Fix flags for OpenBLAS: it uses 32bit ints

--- la/oblas/flags.go
+++ la/oblas/flags.go
@@ -5,13 +5,13 @@
 package oblas
 
 /*
-#cgo linux CFLAGS: -DOPENBLAS_USE64BITINT -O2 -I/usr/local/include
+#cgo linux CFLAGS: -O2 -I/usr/local/include
 #cgo linux LDFLAGS: -lopenblas -L/local/lib
 
-#cgo windows CFLAGS: -DOPENBLAS_USE64BITINT -O2
+#cgo windows CFLAGS: -O2
 #cgo windows LDFLAGS: -lopenblas -lgfortran
 
-#cgo darwin CFLAGS: -DOPENBLAS_USE64BITINT -I/usr/local/opt/openblas/include
+#cgo darwin CFLAGS: -I/usr/local/opt/openblas/include
 #cgo darwin LDFLAGS: -lopenblas -L/usr/local/opt/openblas/lib
 */
 import "C"
