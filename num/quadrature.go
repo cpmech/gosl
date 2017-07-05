cd4c3df381e5f3f4972270779eb425059d44c241
Dorival Pedroso <pedroso@cpmech.com>
2017-07-05 21:22:23 +1000

    Add wrap to qpck function with cosine and sine

--- num/quadrature.go
+++ num/quadrature.go
@@ -24,3 +24,28 @@ func QuadGen(a, b float64, fid int, f func(x float64) float64) (res float64, err
 	res, _, _, _, err = qpck.Agse(id, f, a, b, 0, 0, nil, nil, nil, nil, nil)
 	return
 }
+
+// QuadCs performs automatic integration (quadrature) using the cosine or sine weights
+// QUADPACK routine AWOE (Automatic with weight, Oscillatory)
+//
+//   INPUT:
+//     a      -- lower limit of integration
+//     b      -- upper limit of integration
+//     ω      -- omega
+//     useSin -- use sin(ω⋅x) instead of cos(ω⋅x)
+//     fid    -- index of goroutine (to avoid race problems)
+//     f      -- function defining the integrand
+//
+//   OUTPUT:          b                                     b
+//             res = ∫  f(x) ⋅ cos(ω⋅x) dx     or    res = ∫ f(x) ⋅ sin(ω⋅x) dx
+//                   a                                     a
+//
+func QuadCs(a, b, ω float64, useSin bool, fid int, f func(x float64) float64) (res float64, err error) {
+	id := int32(fid)
+	cs := int32(1) // cos
+	if useSin {
+		cs = 2 // sin
+	}
+	res, _, _, _, err = qpck.Awoe(id, f, a, b, ω, cs, 0, 0, 0, 0, nil, nil, nil, nil, nil, nil, 0, nil)
+	return
+}
--- num/t_quadrature_test.go
+++ num/t_quadrature_test.go
@@ -5,7 +5,7 @@
 package num
 
 import (
-	"math"
+	. "math"
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
@@ -17,7 +17,7 @@ func TestQuadGen01(tst *testing.T) {
 	//verbose()
 	chk.PrintTitle("QuadGen01. using QUADPACK general function")
 
-	f := func(x float64) float64 { return math.Sqrt(1.0 + math.Pow(math.Sin(x), 3.0)) }
+	f := func(x float64) float64 { return Sqrt(1.0 + Pow(Sin(x), 3.0)) }
 	A, err := QuadGen(0, 1, 0, f)
 	if err != nil {
 		tst.Errorf("%v\n", err)
@@ -25,4 +25,21 @@ func TestQuadGen01(tst *testing.T) {
 	}
 	io.Pforan("A  = %v\n", A)
 	chk.Scalar(tst, "A", 1e-12, A, 1.08268158558)
+}
+
+func TestQuadCs01(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("QuadCs01. using QUADPACK oscillatory function")
+
+	ω := Pow(2.0, 3.4)
+	f := func(x float64) float64 { return Exp(20.0 * (x - 1)) }
+	A, err := QuadCs(0, 1, ω, true, 0, f)
+	if err != nil {
+		tst.Errorf("%v\n", err)
+		return
+	}
+	io.Pforan("A  = %v\n", A)
+	Aref := (20*Sin(ω) - ω*Cos(ω) + ω*Exp(-20)) / (Pow(20, 2) + Pow(ω, 2))
+	chk.Scalar(tst, "A", 1e-16, A, Aref)
 }
