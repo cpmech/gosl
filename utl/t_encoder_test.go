21bd35a9bf11c0e3780c7844db2b743350e99a31
Dorival Pedroso <pedroso@cpmech.com>
2017-07-12 00:29:31 +1000

    Rename vars

--- utl/t_encoder_test.go
+++ utl/t_encoder_test.go
@@ -13,17 +13,17 @@ import (
 	goio "io"
 )
 
-type test_writer []byte
-type test_reader []byte
-type test_struct struct{ A, B int }
+type testWriter []byte
+type testReader []byte
+type testStruct struct{ A, B int }
 
-func (o *test_writer) Write(p []byte) (n int, err error) {
+func (o *testWriter) Write(p []byte) (n int, err error) {
 	(*o) = p
 	n = len(p)
 	return
 }
 
-func (o test_reader) Read(p []byte) (n int, err error) {
+func (o testReader) Read(p []byte) (n int, err error) {
 	io.Pforan("p = %v\n", string(p))
 	return 0, goio.EOF
 }
@@ -34,18 +34,18 @@ func Test_encoder01(tst *testing.T) {
 	chk.PrintTitle("encoder01")
 
 	// writer and encoder
-	var w test_writer
+	var w testWriter
 	enc := GetEncoder(&w, "json")
-	s_in := test_struct{A: 1, B: 2}
-	enc.Encode(&s_in)
+	sIn := testStruct{A: 1, B: 2}
+	enc.Encode(&sIn)
 	io.Pforan("w = %q\n", string(w))
 	chk.String(tst, "{\"A\":1,\"B\":2}\n", string(w))
 
 	// reader and decoder
-	var r test_reader
+	var r testReader
 	dec := GetDecoder(&r, "json")
-	var s_out test_struct
-	dec.Decode(&s_out)
+	var sOut testStruct
+	dec.Decode(&sOut)
 	io.Pforan("r = %v\n", r)
 	// TODO: implement test here
 }
