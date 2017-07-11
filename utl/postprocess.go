e1508b4ad71e5b7a72614fb843c13deb3a797a55
Dorival Pedroso <pedroso@cpmech.com>
2017-07-11 23:27:22 +1000

    Fix lint

--- utl/postprocess.go
+++ utl/postprocess.go
@@ -16,26 +16,26 @@ import "math"
 //    tout -- times corresponding to iout
 //  Notes:
 //    use -1 in all_output_times to enforce output of last timestep
-func GetITout(all_output_times, time_stations_out []float64, tol float64) (I []int, T []float64) {
-	lower_index := 0                      // lower index in all_output_times
-	len_aotimes := len(all_output_times)  // length of a_o_times
-	for _, t := range time_stations_out { // for all requested output times
+func GetITout(allOutputTimes, timeStationsOut []float64, tol float64) (I []int, T []float64) {
+	lowerIndex := 0                     // lower index in all_output_times
+	lenAoTimes := len(allOutputTimes)   // length of a_o_times
+	for _, t := range timeStationsOut { // for all requested output times
 		if t < 0 { // final time
-			k := len(all_output_times) - 1     // last output time index
-			I = append(I, k)                   // append index to iout
-			T = append(T, all_output_times[k]) // append last output time
-			continue                           // skip search
+			k := len(allOutputTimes) - 1     // last output time index
+			I = append(I, k)                 // append index to iout
+			T = append(T, allOutputTimes[k]) // append last output time
+			continue                         // skip search
 		}
-		for k := lower_index; k < len_aotimes; k++ { // search within a_o_times
-			if math.Abs(t-all_output_times[k]) < tol { // found near match
-				lower_index += 1                   // update index
-				I = append(I, k)                   // add index to iout
-				T = append(T, all_output_times[k]) // add time to tout
-				break                              // stop searching for this 't'
+		for k := lowerIndex; k < lenAoTimes; k++ { // search within a_o_times
+			if math.Abs(t-allOutputTimes[k]) < tol { // found near match
+				lowerIndex++                     // update index
+				I = append(I, k)                 // add index to iout
+				T = append(T, allOutputTimes[k]) // add time to tout
+				break                            // stop searching for this 't'
 			}
-			if all_output_times[k] > t { // failed to search for 't'
-				lower_index = k // update idx to start from here on
-				break           // skip this 't' and try the next one
+			if allOutputTimes[k] > t { // failed to search for 't'
+				lowerIndex = k // update idx to start from here on
+				break          // skip this 't' and try the next one
 			}
 		}
 	}
