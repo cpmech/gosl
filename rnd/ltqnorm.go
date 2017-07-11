7d2a910336ed1ed745b61a04ead02799561028f8
Dorival Pedroso <pedroso@cpmech.com>
2017-07-12 00:42:17 +1000

    Clean code a little

--- rnd/ltqnorm.go
+++ rnd/ltqnorm.go
@@ -29,7 +29,7 @@ import (
  * C implementation adapted from Peter's Perl version
  */
 
-/* Coefficients in rational approximations. */
+// coefficients in rational approximations
 var (
 	a = []float64{
 		-3.969683028665376e+01,
@@ -63,8 +63,8 @@ var (
 )
 
 func ltqnorm(p float64) float64 {
-	LOW := 0.02425
-	HIGH := 0.97575
+	low := 0.02425
+	hight := 0.97575
 	var q, r float64
 	if p < 0 || p > 1 {
 		chk.Panic("input value (%g) is outside allowed range [0, 1]", p)
@@ -72,22 +72,17 @@ func ltqnorm(p float64) float64 {
 		chk.Panic("p==0 => -infinity")
 	} else if p == 1 {
 		chk.Panic("p==1 => infinity")
-	} else if p < LOW {
-		/* Rational approximation for lower region */
+	} else if p < low {
+		// rational approximation for lower region
 		q = math.Sqrt(-2 * math.Log(p))
-		return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) /
-			((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
-	} else if p > HIGH {
-		/* Rational approximation for upper region */
+		return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
+	} else if p > hight {
+		// rational approximation for upper region
 		q = math.Sqrt(-2 * math.Log(1-p))
-		return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) /
-			((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
-	} else {
-		/* Rational approximation for central region */
-		q = p - 0.5
-		r = q * q
-		return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r + a[5]) * q /
-			(((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r + 1)
+		return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q + c[5]) / ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q + 1)
 	}
-	return 0
+	// rational approximation for central region
+	q = p - 0.5
+	r = q * q
+	return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r + a[5]) * q / (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r + 1)
 }
