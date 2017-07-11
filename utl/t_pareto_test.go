9f816803e1542e5bdc6ac7fb8050b2cadcf17908
Dorival Pedroso <pedroso@cpmech.com>
2017-07-12 00:31:37 +1000

    Rename vars and functions

--- utl/t_pareto_test.go
+++ utl/t_pareto_test.go
@@ -23,14 +23,14 @@ func Test_pareto01(tst *testing.T) {
 	v := []float64{1, 2, 3, 4, 5, 6}
 	io.Pforan("u = %v\n", u)
 	io.Pfblue2("v = %v\n", v)
-	u_dominates, v_dominates := ParetoMin(u, v)
-	io.Pfpink("u_dominates = %v\n", u_dominates)
-	io.Pfpink("v_dominates = %v\n", v_dominates)
-	if u_dominates {
+	uDominates, vDominates := ParetoMin(u, v)
+	io.Pfpink("u_dominates = %v\n", uDominates)
+	io.Pfpink("v_dominates = %v\n", vDominates)
+	if uDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
-	if v_dominates {
+	if vDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
@@ -38,14 +38,14 @@ func Test_pareto01(tst *testing.T) {
 	v = []float64{1, 1.8, 3, 4, 5, 6}
 	io.Pforan("\nu = %v\n", u)
 	io.Pfblue2("v = %v\n", v)
-	u_dominates, v_dominates = ParetoMin(u, v)
-	io.Pfpink("u_dominates = %v\n", u_dominates)
-	io.Pfpink("v_dominates = %v\n", v_dominates)
-	if u_dominates {
+	uDominates, vDominates = ParetoMin(u, v)
+	io.Pfpink("u_dominates = %v\n", uDominates)
+	io.Pfpink("v_dominates = %v\n", vDominates)
+	if uDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
-	if !v_dominates {
+	if !vDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
@@ -53,14 +53,14 @@ func Test_pareto01(tst *testing.T) {
 	v = []float64{1, 2.1, 3, 4, 5, 6}
 	io.Pforan("\nu = %v\n", u)
 	io.Pfblue2("v = %v\n", v)
-	u_dominates, v_dominates = ParetoMin(u, v)
-	io.Pfpink("u_dominates = %v\n", u_dominates)
-	io.Pfpink("v_dominates = %v\n", v_dominates)
-	if !u_dominates {
+	uDominates, vDominates = ParetoMin(u, v)
+	io.Pfpink("u_dominates = %v\n", uDominates)
+	io.Pfpink("v_dominates = %v\n", vDominates)
+	if !uDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
-	if v_dominates {
+	if vDominates {
 		tst.Errorf("test failed\n")
 		return
 	}
@@ -88,41 +88,41 @@ func Test_pareto02(tst *testing.T) {
 
 	U := []float64{1, 2, 3, 4, 5, 6}
 	V := []float64{1, 2, 3, 4, 5, 6}
-	zu, _ := run_pareto_test(U, V, Φ, ntrials)
+	zu, _ := paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 2, 3, 4, 5, 6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 3, 3, 4, 5, 6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 3, 4, 4, 5, 6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 3, 4, 5, 5, 6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 3, 4, 5, 6, 6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{1, 2, 3, 4, 5, 6}
 	V = []float64{2, 3, 4, 5, 6, 7}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	if doplot {
-		plot_pareto_test(&buf, "test_pareto02", Φ, Zu, false, false)
+		plotParetoTest(&buf, "test_pareto02", Φ, Zu, false, false)
 	}
 }
 
@@ -148,69 +148,69 @@ func Test_pareto03(tst *testing.T) {
 
 	U := []float64{-1, -2, -3, -4, -5, -6}
 	V := []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ := run_pareto_test(U, V, Φ, ntrials)
+	zu, _ := paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -2, -3, -4, -5, -6}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -3, -3, -4, -5, -6}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -3, -4, -4, -5, -6}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -3, -4, -5, -5, -6}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -3, -4, -5, -6, -6}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	U = []float64{-2, -3, -4, -5, -6, -7}
 	V = []float64{-1, -2, -3, -4, -5, -6}
-	zu, _ = run_pareto_test(U, V, Φ, ntrials)
+	zu, _ = paretoTest(U, V, Φ, ntrials)
 	Zu = append(Zu, zu...)
 
 	if doplot {
-		plot_pareto_test(&buf, "test_pareto03", Φ, Zu, false, true)
+		plotParetoTest(&buf, "test_pareto03", Φ, Zu, false, true)
 	}
 }
 
-func run_pareto_test(U, V, Φ []float64, ntrials int) (zu, zv []float64) {
+func paretoTest(U, V, Φ []float64, ntrials int) (zu, zv []float64) {
 	io.Pforan("\nu = %v\n", U)
 	io.Pforan("v = %v\n", V)
 	io.PfWhite("%5s%13s%13s\n", "φ", "u wins", "v wins")
 	zu = make([]float64, len(Φ))
 	zv = make([]float64, len(Φ))
 	for i, φ := range Φ {
-		u_wins := 0
-		v_wins := 0
+		uWins := 0
+		vWins := 0
 		for j := 0; j < ntrials; j++ {
-			u_dominates := ParetoMinProb(U, V, φ)
-			if u_dominates {
-				u_wins++
+			uDominates := ParetoMinProb(U, V, φ)
+			if uDominates {
+				uWins++
 			} else {
-				v_wins++
+				vWins++
 			}
 		}
-		zu[i] = 100 * float64(u_wins) / float64(ntrials)
-		zv[i] = 100 * float64(v_wins) / float64(ntrials)
+		zu[i] = 100 * float64(uWins) / float64(ntrials)
+		zv[i] = 100 * float64(vWins) / float64(ntrials)
 		io.Pf("%5.2f%12.2f%%%12.2f%%\n", φ, zu[i], zv[i])
 	}
 	return
 }
 
-func write_python_array(buf *bytes.Buffer, name string, x []float64) {
+func writePythonArray(buf *bytes.Buffer, name string, x []float64) {
 	io.Ff(buf, "%s=np.array([", name)
 	for i := 0; i < len(x); i++ {
 		io.Ff(buf, "%g,", x[i])
@@ -218,7 +218,7 @@ func write_python_array(buf *bytes.Buffer, name string, x []float64) {
 	io.Ff(buf, "])\n")
 }
 
-func write_python_matrix(buf *bytes.Buffer, name string, xy [][]float64) {
+func writePythonMatrix(buf *bytes.Buffer, name string, xy [][]float64) {
 	io.Ff(buf, "%s=np.array([", name)
 	for i := 0; i < len(xy); i++ {
 		io.Ff(buf, "[")
@@ -230,15 +230,15 @@ func write_python_matrix(buf *bytes.Buffer, name string, xy [][]float64) {
 	io.Ff(buf, "])\n")
 }
 
-func plot_pareto_test(buf *bytes.Buffer, fnkey string, Φ, Zu []float64, show, negative bool) {
+func plotParetoTest(buf *bytes.Buffer, fnkey string, Φ, Zu []float64, show, negative bool) {
 	io.Ff(buf, "from gosl import SetForEps, Save\n")
 	io.Ff(buf, "from mpl_toolkits.mplot3d import Axes3D\n")
 	io.Ff(buf, "import matplotlib.pyplot as plt\n")
 	io.Ff(buf, "import numpy as np\n")
 	io.Ff(buf, "fig = plt.figure()\n")
 	io.Ff(buf, "ax = fig.add_subplot(111, projection='3d')\n")
-	write_python_array(buf, "phi", Φ)
-	write_python_array(buf, "dz", Zu)
+	writePythonArray(buf, "phi", Φ)
+	writePythonArray(buf, "dz", Zu)
 	io.Ff(buf, "SetForEps(0.75, 455, mplclose=0, text_usetex=0)\n")
 	io.Ff(buf, "n=len(phi)\nx,y=np.meshgrid(phi,np.linspace(0,1,7))\nx=x.flatten()\ny=y.flatten()\nz=np.zeros(n*7)\n")
 	io.Ff(buf, "ax.bar3d(x,y,z,dx=0.1*np.ones(n*7),dy=0.1*np.ones(n*7),dz=dz, color='#cee9ff')\n")
@@ -292,9 +292,9 @@ func Test_pareto04(tst *testing.T) {
 		io.Ff(&buf, "import matplotlib.pyplot as plt\n")
 		io.Ff(&buf, "import numpy as np\n")
 		io.Ff(&buf, "SetForEps(0.75, 355)\n")
-		write_python_matrix(&buf, "XY", ovs)
-		write_python_array(&buf, "Xp", Xp)
-		write_python_array(&buf, "Yp", Yp)
+		writePythonMatrix(&buf, "XY", ovs)
+		writePythonArray(&buf, "Xp", Xp)
+		writePythonArray(&buf, "Yp", Yp)
 		io.Ff(&buf, "plt.plot(XY[:,0],XY[:,1],'r.',clip_on=0)\n")
 		io.Ff(&buf, "plt.plot(Xp,Yp,'ko',markerfacecolor='none',ms=7,clip_on=0)\n")
 		io.Ff(&buf, "Gll('x','y','')\n")
