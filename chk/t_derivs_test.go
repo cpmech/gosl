d944641464cd4b290cdf6fdcf174085855834d0c
Dorival Pedroso <pedroso@cpmech.com>
2017-07-17 13:59:24 +1000

    Add tests for conditions in derivs functions

--- chk/t_derivs_test.go
+++ chk/t_derivs_test.go
@@ -9,10 +9,10 @@ import (
 	"testing"
 )
 
-func Test_deriv01(tst *testing.T) {
+func TestDeriv01(tst *testing.T) {
 
 	//Verbose = true
-	PrintTitle("deriv01. DerivScaSca")
+	PrintTitle("Deriv01. DerivScaSca")
 
 	f := func(x float64) (float64, error) { return math.Cos(math.Pi * x / 2.0), nil }
 
@@ -35,10 +35,10 @@ func Test_deriv01(tst *testing.T) {
 	}
 }
 
-func Test_deriv02(tst *testing.T) {
+func TestDeriv02(tst *testing.T) {
 
 	//Verbose = true
-	PrintTitle("deriv02. DerivVecSca")
+	PrintTitle("Deriv02. DerivVecSca")
 
 	fcn := func(f []float64, x float64) (err error) {
 		f[0] = math.Cos(math.Pi * x / 2.0)
@@ -91,10 +91,10 @@ func Test_deriv02(tst *testing.T) {
 	}
 }
 
-func Test_deriv03(tst *testing.T) {
+func TestDeriv03(tst *testing.T) {
 
 	//Verbose = true
-	PrintTitle("deriv03. DerivScaVec")
+	PrintTitle("Deriv03. DerivScaVec")
 
 	fcn := func(x []float64) (float64, error) {
 		return x[0]*x[0]*x[0] + x[1]*x[1] + x[0]*x[1] + x[0] - x[1], nil
@@ -136,10 +136,10 @@ func Test_deriv03(tst *testing.T) {
 	}
 }
 
-func Test_deriv04(tst *testing.T) {
+func TestDeriv04(tst *testing.T) {
 
 	//Verbose = true
-	PrintTitle("deriv04. DerivVecVec")
+	PrintTitle("Deriv04. DerivVecVec")
 
 	fcn := func(f, x []float64) (err error) {
 		f[0] = x[0]*x[0]*x[0] + x[1]*x[1] + x[0]*x[1] + x[0] - x[1]
@@ -168,7 +168,7 @@ func Test_deriv04(tst *testing.T) {
 	t2 := new(testing.T)
 	DerivVecVec(t2, "dfdx", 2.0e-11, dfdxAna, xAt, dx, Verbose, fcn)
 	if t2.Failed() {
-		tst.Errorf("t2 should not have failed\n")
+		tst.Errorf("t2 should NOT have failed\n")
 		return
 	}
 
@@ -178,7 +178,93 @@ func Test_deriv04(tst *testing.T) {
 	t3 := new(testing.T)
 	DerivVecVec(t3, "dfdx", 1e-15, dfdxAna, xAt, dx, Verbose, fcn)
 	if t3.Failed() {
-		tst.Errorf("t3 should not have failed\n")
+		tst.Errorf("t3 should NOT have failed\n")
 		return
+	}
+}
+
+func TestDeriv05(tst *testing.T) {
+
+	//Verbose = true
+	PrintTitle("Deriv05. Deriv methods. error messages")
+
+	t1 := new(testing.T)
+	DerivScaVec(t1, "", 0, []float64{1, 2, 3}, []float64{1}, 0, false, nil)
+	if !t1.Failed() {
+		tst.Errorf("t1 should have failed")
+	}
+
+	t2 := new(testing.T)
+	DerivVecVec(t2, "", 0, nil, []float64{1}, 0, false, nil)
+	if !t2.Failed() {
+		tst.Errorf("t2 should have failed")
+	}
+
+	t3 := new(testing.T)
+	DerivVecVec(t3, "", 0, [][]float64{{1, 2}, {2, 1}, {3, 4}}, []float64{1}, 0, false, nil)
+	if !t3.Failed() {
+		tst.Errorf("t3 should have failed")
+	}
+
+	t4 := new(testing.T)
+	DerivVecVec(t4, "", 0, [][]float64{{1}, {2, 1}, {3, 4}}, []float64{1}, 1e-3, false, func(f, x []float64) error { return nil })
+	if !t4.Failed() {
+		tst.Errorf("t4 should have failed")
+	}
+
+	t5 := new(testing.T)
+	DerivScaSca(t5, "", 0, 0, 0, 1e-3, false, func(x float64) (float64, error) { return 0, Err("stop") })
+	if !t5.Failed() {
+		tst.Errorf("t5 should have failed")
+	}
+
+	t6 := new(testing.T)
+	xAt, h := 1.0, 1.0
+	DerivScaSca(t6, "", 0, 0, xAt, h, false, func(x float64) (float64, error) {
+		if x == 2.0 { // xAt + h
+			return 0, Err("stop")
+		}
+		return x, nil
+	})
+	if !t6.Failed() {
+		tst.Errorf("t6 should have failed")
+	}
+
+	t7 := new(testing.T)
+	DerivScaSca(t7, "", 0, 0, xAt, h, false, func(x float64) (float64, error) {
+		if x == 0.5 { // xAt - h/2
+			return 0, Err("stop")
+		}
+		return x, nil
+	})
+	if !t7.Failed() {
+		tst.Errorf("t7 should have failed")
+	}
+
+	t8 := new(testing.T)
+	DerivScaSca(t8, "", 0, 0, xAt, h, false, func(x float64) (float64, error) {
+		if x == 1.5 { // xAt + h/2
+			return 0, Err("stop")
+		}
+		return x, nil
+	})
+	if !t8.Failed() {
+		tst.Errorf("t8 should have failed")
+	}
+
+	k := 0
+	t9 := new(testing.T)
+	DerivScaSca(t9, "", 0, 0, xAt, h, false, func(x float64) (float64, error) {
+		if x == 1.5 { // use discrete function to trigger "check rounding error" condition
+			k++
+			return 10.0, nil
+		}
+		if k > 0 {
+			return 0, Err("stop")
+		}
+		return 0, nil
+	})
+	if !t9.Failed() {
+		tst.Errorf("t9 should have failed")
 	}
 }
--- chk/xmonitor.bash
+++ chk/xmonitor.bash
@@ -8,5 +8,5 @@ while true; do
     echo
     echo
     echo
-    go test -test.run="MinMax01"
+    go test -test.run="Deriv05"
 done
