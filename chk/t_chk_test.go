9846b252e5572ab6ee18c64dcba412f0df0f088c
Dorival Pedroso <pedroso@cpmech.com>
2017-07-17 14:15:43 +1000

    Add more tests

--- chk/t_chk_test.go
+++ chk/t_chk_test.go
@@ -4,13 +4,51 @@
 
 package chk
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Test_err01(tst *testing.T) {
+func TestET(tst *testing.T) {
 
 	//Verbose = true
-	defer func() { Verbose = false }()
-	PrintTitle("err01")
+	PrintTitle("ET (error test)")
+
+	t1 := new(testing.T)
+	ET(t1, nil)
+	if t1.Failed() {
+		tst.Errorf("t1 should NOT have failed\n")
+	}
+
+	err := Err("stop")
+	t2 := new(testing.T)
+	ET(t2, err)
+	if !t2.Failed() {
+		tst.Errorf("t2 should have failed\n")
+	}
+}
+
+func TestEP(tst *testing.T) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			if Verbose {
+				fmt.Printf("OK, caught the following message:\n\n\t%v\n", err)
+			}
+		} else {
+			tst.Errorf("\n\tTEST FAILED. test should have panicked\n")
+		}
+	}()
+
+	//Verbose = true
+	PrintTitle("EP (error panic)")
+	EP(Err("stop"))
+}
+
+func TestErr01(tst *testing.T) {
+
+	//Verbose = true
+	PrintTitle("Err01")
 
 	err := Err("TEST_ERR = %d", 666)
 	if err.Error() != "TEST_ERR = 666" {
@@ -19,4 +57,27 @@ func Test_err01(tst *testing.T) {
 
 	PrintOk("hello from PrintOk => ")
 	PrintFail("hello from PrintFail => ")
+}
+
+func TestCallerInfo(tst *testing.T) {
+	//Verbose = true
+	PrintTitle("CallerInfo")
+	CallerInfo(100)
+}
+
+func TestPanicErr(tst *testing.T) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			if Verbose {
+				fmt.Printf("OK, caught the following message:\n\n\t%v\n", err)
+			}
+		} else {
+			tst.Errorf("\n\tTEST FAILED. test should have panicked\n")
+		}
+	}()
+
+	//Verbose = true
+	PrintTitle("PanicErr")
+	PanicErr(Err("stop"))
 }
