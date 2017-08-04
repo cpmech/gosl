18e658628dc120d1c5743812ef5a48559ac2445e
Dorival Pedroso <pedroso@cpmech.com>
2017-08-04 19:17:30 +1000

    Use variable to save results and prevent optimization errors

--- fun/t_b_imagpown_test.go
+++ fun/t_b_imagpown_test.go
@@ -9,36 +9,48 @@ import (
 	"testing"
 )
 
+var (
+	benchmarkingResC complex128
+)
+
 func BenchmarkImagPowN(b *testing.B) {
+	var res complex128
 	for i := 0; i < b.N; i++ {
 		for n := 0; n < 200; n++ {
-			ImagPowN(n)
+			res = ImagPowN(n)
 		}
 	}
+	benchmarkingResC = res
 }
 
 func BenchmarkImagPowNcmplx(b *testing.B) {
+	var res complex128
 	for i := 0; i < b.N; i++ {
 		for n := 0; n < 200; n++ {
-			cmplx.Pow(1i, complex(float64(n), 0))
+			res = cmplx.Pow(1i, complex(float64(n), 0))
 		}
 	}
+	benchmarkingResC = res
 }
 
 func BenchmarkImagXpowN(b *testing.B) {
+	var res complex128
 	x := 2.5
 	for i := 0; i < b.N; i++ {
 		for n := 0; n < 200; n++ {
-			ImagXpowN(x, n)
+			res = ImagXpowN(x, n)
 		}
 	}
+	benchmarkingResC = res
 }
 
 func BenchmarkImagXpowNcmplx(b *testing.B) {
+	var res complex128
 	x := 2.5
 	for i := 0; i < b.N; i++ {
 		for n := 0; n < 200; n++ {
-			cmplx.Pow(complex(0, x), complex(float64(n), 0))
+			res = cmplx.Pow(complex(0, x), complex(float64(n), 0))
 		}
 	}
+	benchmarkingResC = res
 }
