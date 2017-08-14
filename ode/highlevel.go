4b9adfefb05f094cf27e13fdc83a30423a738250
Dorival Pedroso <pedroso@cpmech.com>
2017-08-14 17:34:15 +1000

    Implement high-level function. Add VDP test with cycle

---
+++ ode/highlevel.go
@@ -0,0 +1,76 @@
+// Copyright 2016 The Gosl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ode
+
+import "github.com/cpmech/gosl/la"
+
+// Solve solves ODE problem using standard parameters
+//
+//  INPUT:
+//   method    -- the method
+//   fcn       -- function d{y}/dx := {f}(h=dx, x, {y})
+//   jac       -- Jacobian function d{f}/d{y} := [J](h=dx, x, {y}) [may be nil]
+//   y         -- current {y} @ x=0
+//   xf        -- final x
+//   dx        -- stepsize. [may be used for continuous output]
+//   atol      -- absolute tolerance; use 0 for default [default = 1e-4] (for fixedStp=false)
+//   rtol      -- relative tolerance; use 0 for default [default = 1e-4] (for fixedStp=false)
+//   numJac    -- use numerical Jacobian if if jac is non nil
+//   fixedStep -- fixed steps
+//   saveStep  -- save steps
+//   saveCont  -- save continuously [using dx]
+//
+//  OUTPUT:
+//   yf   -- final {y}
+//   stat -- statistics
+//   out  -- output with all steps results with save==true
+//
+func Solve(method string, fcn Func, jac JacF, y la.Vector, xf, dx, atol, rtol float64,
+	numJac, fixedStep, saveStep, saveCont bool) (yf la.Vector, stat *Stat, out *Output, err error) {
+
+	// current y vector
+	ndim := len(y)
+	yf = la.NewVector(ndim)
+	yf.Apply(1, y)
+
+	// configuration
+	conf, err := NewConfig(method, "", nil)
+	if err != nil {
+		return
+	}
+	if atol > 0 && rtol > 0 {
+		conf.SetTol(atol, rtol)
+	}
+	if fixedStep {
+		conf.SetFixedH(dx, xf)
+	}
+	if saveStep {
+		conf.SetStepOut(true, nil)
+	}
+	if saveCont {
+		conf.SetContOut(true, dx, xf, nil)
+	}
+
+	// output handler
+	out = NewOutput(ndim, conf)
+
+	// allocate solver
+	J := jac
+	if numJac {
+		J = nil
+	}
+	sol, err := NewSolver(ndim, conf, out, fcn, J, nil)
+	if err != nil {
+		return
+	}
+	defer sol.Free()
+
+	// solve ODE
+	err = sol.Solve(yf, 0.0, xf)
+
+	// set stat variable
+	stat = sol.Stat
+	return
+}
--- ode/t_erk_test.go
+++ ode/t_erk_test.go
@@ -277,7 +277,7 @@ func TestErk03b(tst *testing.T) {
 func TestErk04(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("Erk04.")
+	chk.PrintTitle("Erk04. problem a. methods with p<5")
 
 	// problem
 	p := ProbSimpleNdim4a()
@@ -306,5 +306,48 @@ func TestErk04(tst *testing.T) {
 		plt.SetXlog()
 		plt.SetYlog()
 		plt.Save("/tmp/gosl/ode", "erk04")
+	}
+}
+
+func TestErk05(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("Erk05. Van der Pol with 1 cycle/stationary point")
+
+	// constants
+	T := 6.6632868593231301896996820305
+	A := 2.00861986087484313650940188
+	y := la.Vector([]float64{A, 0})
+	xf := T
+	dx := 0.1
+
+	// function
+	fcn := func(f la.Vector, dx, x float64, y la.Vector) error {
+		f[0] = y[1]
+		f[1] = (1.0-y[0]*y[0])*y[1] - y[0]
+		return nil
+	}
+
+	// solve
+	atol, rtol := 1e-5, 1e-5
+	numJac, fixedStep, saveStep, saveCont := false, false, true, false
+	yf, stat, out, err := Solve("dopri5", fcn, nil, y, xf, dx, atol, rtol, numJac, fixedStep, saveStep, saveCont)
+	status(tst, err)
+
+	// results
+	io.Pf("yf = %v\n", yf)
+	stat.Print(false)
+
+	// check
+	chk.AnaNum(tst, "y0", 1e-4, yf[0], y[0], chk.Verbose)
+	chk.AnaNum(tst, "y1", 1e-4, yf[1], y[1], chk.Verbose)
+
+	// plot
+	if chk.Verbose {
+		plt.Reset(true, nil)
+		plt.Plot(out.GetStepY(0), out.GetStepY(1), &plt.A{M: ".", C: plt.C(0, 0), NoClip: true})
+		plt.Gll("$y_0$", "$y_1$", nil)
+		plt.Equal()
+		plt.Save("/tmp/gosl/ode", "erk05")
 	}
 }
