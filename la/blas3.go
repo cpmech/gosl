152414e621505188e498c47908607c587a251734
Dorival Pedroso <pedroso@cpmech.com>
2017-08-22 18:18:23 +1000

    Implement functions to compute mat-mat multiplication (+add)

--- la/blas3.go
+++ la/blas3.go
@@ -20,12 +20,80 @@ func MatMatMul(c *Matrix, α float64, a, b *Matrix) {
 	}
 }
 
-// MatMatMulAdd returns the matrix multiplication (scaled) with addition
+// MatTrMatMul returns the matrix multiplication (scaled) with transposed(a)
+//
+//  c := α⋅aᵀ⋅b    ⇒    cij := α * aki * bkj
+//
+func MatTrMatMul(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(true, false, a.N, b.N, a.M, α, a.Data, a.M, b.Data, b.M, 0.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// MatMatTrMul returns the matrix multiplication (scaled) with transposed(b)
+//
+//  c := α⋅a⋅bᵀ    ⇒    cij := α * aik * bjk
+//
+func MatMatTrMul(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(false, true, a.M, b.M, a.N, α, a.Data, a.M, b.Data, b.M, 0.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// MatTrMatTrMul returns the matrix multiplication (scaled) with transposed(a) and transposed(b)
+//
+//  c := α⋅aᵀ⋅bᵀ    ⇒    cij := α * aki * bjk
+//
+func MatTrMatTrMul(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(true, true, a.N, b.M, a.M, α, a.Data, a.M, b.Data, b.M, 0.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// mat mul add ////////////////////////////////////////////////////////////////////////////////////
+
+// MatMatMulAdd returns the matrix multiplication (scaled)
 //
 //  c += α⋅a⋅b    ⇒    cij += α * aik * bkj
 //
 func MatMatMulAdd(c *Matrix, α float64, a, b *Matrix) {
 	err := oblas.Dgemm(false, false, a.M, b.N, a.N, α, a.Data, a.M, b.Data, b.M, 1.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// MatTrMatMulAdd returns the matrix multiplication (scaled) with transposed(a)
+//
+//  c += α⋅aᵀ⋅b    ⇒    cij += α * aki * bkj
+//
+func MatTrMatMulAdd(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(true, false, a.N, b.N, a.M, α, a.Data, a.M, b.Data, b.M, 1.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// MatMatTrMulAdd returns the matrix multiplication (scaled) with transposed(b)
+//
+//  c += α⋅a⋅bᵀ    ⇒    cij += α * aik * bjk
+//
+func MatMatTrMulAdd(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(false, true, a.M, b.M, a.N, α, a.Data, a.M, b.Data, b.M, 1.0, c.Data, c.M)
+	if err != nil {
+		chk.Panic("%v\n", err)
+	}
+}
+
+// MatTrMatTrMulAdd returns the matrix multiplication (scaled) with transposed(a) and transposed(b)
+//
+//  c += α⋅aᵀ⋅bᵀ    ⇒    cij += α * aki * bjk
+//
+func MatTrMatTrMulAdd(c *Matrix, α float64, a, b *Matrix) {
+	err := oblas.Dgemm(true, true, a.N, b.M, a.M, α, a.Data, a.M, b.Data, b.M, 1.0, c.Data, c.M)
 	if err != nil {
 		chk.Panic("%v\n", err)
 	}
--- la/t_blas3_test.go
+++ la/t_blas3_test.go
@@ -13,34 +13,95 @@ import (
 func TestBlas3tst01(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("Blas3tst01. (real) Blas3 functions")
+	chk.PrintTitle("Blas3tst01. (real) MatMat mul functions")
 
 	// allocate data
-	a := NewMatrixDeep2([][]float64{
+	a := NewMatrixDeep2([][]float64{ // 2 x 3
 		{1.0, 2.00, 3.0},
 		{0.5, 0.75, 1.5},
 	})
-	b := NewMatrixDeep2([][]float64{
+	b := NewMatrixDeep2([][]float64{ // 3 x 4
 		{0.1, 0.5, 0.5, 0.75},
 		{0.2, 2.0, 2.0, 2.0},
 		{0.3, 0.5, 0.5, 0.5},
 	})
+	cref := [][]float64{
+		{2.80, 12.0, 12.0, 12.50},
+		{1.30, +5.0, +5.0, +5.25},
+	}
+
+	// tranpose matrices
+	atrans := a.GetTranspose()
+	btrans := b.GetTranspose()
 
 	// MatMatMul
 	c := NewMatrix(2, 4)
-	c.Set(0, 0, 12333)
-	c.Set(1, 3, 111)
-	MatMatMul(c, 1, a, b)
-	chk.Deep2(tst, "c := 1⋅a⋅b", 1e-15, c.GetDeep2(), [][]float64{
-		{1.40, 6.0, 6.0, 6.250},
-		{0.65, 2.5, 2.5, 2.625},
+	c.Set(0, 0, 12333) // noise
+	c.Set(1, 3, 111)   // noise
+	MatMatMul(c, 2, a, b)
+	chk.Deep2(tst, "c := 2⋅a⋅b", 1e-17, c.GetDeep2(), cref)
+
+	// MatMatTrMul
+	c.Set(0, 0, 12333) // noise
+	c.Set(1, 3, 111)   // noise
+	MatTrMatMul(c, 2, atrans, b)
+	chk.Deep2(tst, "c := 2⋅aᵀ⋅b", 1e-17, c.GetDeep2(), cref)
+
+	// MatMatTrMul
+	c.Set(0, 0, 12333) // noise
+	c.Set(1, 3, 111)   // noise
+	MatMatTrMul(c, 2, a, btrans)
+	chk.Deep2(tst, "c := 2⋅a⋅bᵀ", 1e-17, c.GetDeep2(), cref)
+
+	// MatTrMatTrMul
+	c.Set(0, 0, 12333) // noise
+	c.Set(1, 3, 111)   // noise
+	MatTrMatTrMul(c, 2, atrans, btrans)
+	chk.Deep2(tst, "c := 2⋅aᵀ⋅bᵀ", 1e-17, c.GetDeep2(), cref)
+}
+
+func TestBlas3tst02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("Blas3tst02. (real) MatMat mul + add functions")
+
+	// allocate data
+	a := NewMatrixDeep2([][]float64{ // 2 x 3
+		{1.0, 2.00, 3.0},
+		{0.5, 0.75, 1.5},
 	})
+	b := NewMatrixDeep2([][]float64{ // 3 x 4
+		{0.1, 0.5, 0.5, 0.75},
+		{0.2, 2.0, 2.0, 2.0},
+		{0.3, 0.5, 0.5, 0.5},
+	})
+	cref := [][]float64{
+		{102.80, 112.0, 112.0, 112.50},
+		{101.30, 105.0, 105.0, 105.25},
+	}
+
+	// tranpose matrices
+	atrans := a.GetTranspose()
+	btrans := b.GetTranspose()
 
 	// MatMatMulAdd
+	c := NewMatrix(2, 4)
 	c.Fill(100)
-	MatMatMulAdd(c, 1, a, b)
-	chk.Deep2(tst, "c := 1⋅a⋅b", 1e-17, c.GetDeep2(), [][]float64{
-		{101.40, 106.0, 106.0, 106.250},
-		{100.65, 102.5, 102.5, 102.625},
-	})
+	MatMatMulAdd(c, 2, a, b)
+	chk.Deep2(tst, "c := 2⋅a⋅b + 100", 1e-17, c.GetDeep2(), cref)
+
+	// MatMatTrMulAdd
+	c.Fill(100)
+	MatTrMatMulAdd(c, 2, atrans, b)
+	chk.Deep2(tst, "c := 2⋅aᵀ⋅b + 100", 1e-17, c.GetDeep2(), cref)
+
+	// MatMatTrMulAdd
+	c.Fill(100)
+	MatMatTrMulAdd(c, 2, a, btrans)
+	chk.Deep2(tst, "c := 2⋅a⋅bᵀ + 100", 1e-17, c.GetDeep2(), cref)
+
+	// MatTrMatTrMulAdd
+	c.Fill(100)
+	MatTrMatTrMulAdd(c, 2, atrans, btrans)
+	chk.Deep2(tst, "c := 2⋅aᵀ⋅bᵀ + 100", 1e-17, c.GetDeep2(), cref)
 }
