3456291301ba6843412e7f34a289824631eaf958
Dorival Pedroso <pedroso@cpmech.com>
2017-11-06 11:25:56 +1000

    Optimise some blas1 functions

--- la/blas1.go
+++ la/blas1.go
@@ -35,7 +35,8 @@ func VecRmsError(u, v Vector, a, m float64, s Vector) (rms float64) {
 // VecDot returns the dot product between two vectors:
 //   s := u・v
 func VecDot(u, v Vector) (res float64) {
-	if len(u) < 9 {
+	cutoff := 150
+	if len(u) <= cutoff {
 		for i := 0; i < len(u); i++ {
 			res += u[i] * v[i]
 		}
@@ -47,9 +48,24 @@ func VecDot(u, v Vector) (res float64) {
 // VecAdd adds the scaled components of two vectors
 //   res := α⋅u + β⋅v   ⇒   result[i] := α⋅u[i] + β⋅v[i]
 func VecAdd(res Vector, α float64, u Vector, β float64, v Vector) {
-	for i := 0; i < len(u); i++ {
+	n := len(u)
+	cutoff := 150
+	if β == 1 && n > cutoff {
+		copy(res, v)
+		oblas.Daxpy(n, α, u, 1, res, 1)
+		return
+	}
+	m := n % 4
+	for i := 0; i < m; i++ {
 		res[i] = α*u[i] + β*v[i]
 	}
+	for i := m; i < n; i += 4 {
+		res[i+0] = α*u[i+0] + β*v[i+0]
+		res[i+1] = α*u[i+1] + β*v[i+1]
+		res[i+2] = α*u[i+2] + β*v[i+2]
+		res[i+3] = α*u[i+3] + β*v[i+3]
+	}
+	return
 }
 
 // VecMaxDiff returns the maximum absolute difference between two vectors
--- la/t_blas1_test.go
+++ la/t_blas1_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/io"
 )
 
 func TestBlas1tst01(tst *testing.T) {
@@ -32,14 +33,42 @@ func TestBlas1tst01(tst *testing.T) {
 
 	// VecAdd
 	w := NewVector(len(u))
+	W := NewVector(len(U))
 	VecAdd(w, 1, u, -2, v)
+	VecAdd(W, 1, U, -1, V)
 	chk.Array(tst, "w := 1⋅u - 2⋅v", 1e-17, w, []float64{-5, -2, 1})
+	chk.Array(tst, "W := 1⋅U - 1⋅V", 1e-17, W, []float64{-15, -13, -11, -9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11, 13, 15})
 
 	// VecMaxDiff
 	chk.Float64(tst, "VecMaxDiff(u, w)", 1e-17, VecMaxDiff(u, w), 6.0)
+	u[1] = 122
+	chk.Float64(tst, "VecMaxDiff(u, v)", 1e-17, VecMaxDiff(u, v), 120)
 
 	// VecScaleAbs
 	scale := NewVector(len(w))
 	VecScaleAbs(scale, -1, 2, w)
 	chk.Array(tst, "scale := -1 + 2⋅w", 1e-17, scale, []float64{9, 3, 1})
+}
+
+func TestBlas1tst02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("Blas1tst02. (real) Blas1 functions. Larger vectors")
+
+	u := make([]float64, 200)
+	v := make([]float64, 200)
+	w := make([]float64, 200)
+	wref := make([]float64, 200)
+	for _, n := range []int{7, 8, 150, 151} {
+		dot := 0.0
+		for i := 0; i < n; i++ {
+			u[i] = 2 * float64(1+i)
+			v[i] = -float64(1 + i)
+			wref[i] = u[i] + v[i]
+			dot += u[i] * v[i]
+		}
+		VecAdd(w[:n], 1, u[:n], 1, v[:n])
+		chk.Array(tst, io.Sf("n=%3d: w:=u-v", n), 1e-15, w[:n], wref[:n])
+		chk.Float64(tst, "u⋅v", 1e-15, VecDot(u, v), dot)
+	}
 }
