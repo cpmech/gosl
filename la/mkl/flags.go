b49dba722bfc74ad99b0396955c93e484d3e1c32
Dorival Pedroso <pedroso@cpmech.com>
2018-02-21 10:58:02 -0800

    Remove unallowed flags due to Go 1.10

--- la/mkl/flags.go
+++ la/mkl/flags.go
@@ -5,7 +5,7 @@
 package mkl
 
 /*
-#cgo CFLAGS: -O2 -DMKL_ILP64 -m64 -I/opt/intel/mkl/include
+#cgo CFLAGS: -O2 -DMKL_ILP64 -I/opt/intel/mkl/include
 #cgo LDFLAGS: -Wl,--start-group /opt/intel/mkl/lib/intel64/libmkl_intel_ilp64.a /opt/intel/mkl/lib/intel64/libmkl_intel_thread.a /opt/intel/mkl/lib/intel64/libmkl_core.a /opt/intel/lib/intel64/libiomp5.a -Wl,--end-group -lpthread -lm -ldl -L/opt/intel/lib/intel64
 */
 import "C"
