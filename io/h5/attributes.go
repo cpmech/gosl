74eb27cfb55728cf33d6f6df1f5339f60a55b197
Dorival Pedroso <pedroso@cpmech.com>
2018-03-08 17:31:13 -0800

    Move methods about int attributes to attributes file

--- io/h5/attributes.go
+++ io/h5/attributes.go
@@ -138,3 +138,85 @@ func (o *File) IntReadAttr(path, key string) (val int) {
 	}
 	return vals[0]
 }
+
+// Ints /////////////////////////////////////////////////////////////////////////////////////////
+
+// IntsSetAttr sets ints attibute
+func (o *File) IntsSetAttr(path, key string, vals []int) {
+
+	// GOB
+	if o.useGob {
+		if o.gobReading {
+			chk.Panic("cannot put %q because file is open for READONLY", path)
+		}
+		o.gobEnc.Encode("IntsSetAttr")
+		o.gobEnc.Encode(path)
+		o.gobEnc.Encode(key)
+		o.gobEnc.Encode(vals)
+		return
+	}
+
+	// HDF5
+	ckey := C.CString(key)
+	defer C.free(unsafe.Pointer(ckey))
+	n := C.size_t(len(vals))
+	o.hierarchCreate(path, func(cp *C.char) C.herr_t {
+		st := C.H5LTset_attribute_long(o.hdfHandle, cp, ckey, (*C.long)(unsafe.Pointer(&vals[0])), n)
+		if st < 0 {
+			chk.Panic("cannot set attibute %q to vals in path=%q", key, path)
+		}
+		return 0
+	})
+}
+
+// IntsReadAttr reads ints attribute
+func (o *File) IntsReadAttr(path, key string) (vals []int) {
+
+	// GOB
+	if o.useGob {
+		var cmd string
+		o.gobDec.Decode(&cmd)
+		if cmd != "IntsSetAttr" {
+			chk.Panic("wrong command => %q\n(r/w commands need to be called in the same order)", cmd)
+		}
+		var rpath string
+		o.gobDec.Decode(&rpath)
+		if rpath != path {
+			chk.Panic("cannot read path: %s != %s\n(r/w commands need to be called in the same order)", path, rpath)
+		}
+		var rkey string
+		o.gobDec.Decode(&rkey)
+		if rkey != key {
+			chk.Panic("cannot read key: %s != %s\n(r/w commands need to be called in the same order)", key, rkey)
+		}
+		o.gobDec.Decode(&vals)
+		return
+	}
+
+	// HDF5
+	o.filterPath(path)
+	cpth, ckey := C.CString(path), C.CString(key)
+	defer C.free(unsafe.Pointer(cpth))
+	defer C.free(unsafe.Pointer(ckey))
+	var rank int
+	st := C.H5LTget_attribute_ndims(o.hdfHandle, cpth, ckey, (*C.int)(unsafe.Pointer(&rank))) //unsafe.Pointer(&rank[0])))
+	if st < 0 {
+		chk.Panic("cannot read attibute %q from rank in path=%q", key, path)
+	}
+	if rank != 1 {
+		chk.Panic("cannot read attibute %q because rank == %d != 1. path=%q", key, rank, path)
+	}
+	var typeClass C.H5T_class_t
+	var typeSize C.size_t
+	dims := make([]int, rank)
+	st = C.H5LTget_attribute_info(o.hdfHandle, cpth, ckey, (*C.hsize_t)(unsafe.Pointer(&dims[0])), &typeClass, &typeSize)
+	if st < 0 {
+		chk.Panic("cannot read attibute %q from dims in path=%q", key, path)
+	}
+	vals = make([]int, dims[0])
+	st = C.H5LTget_attribute_long(o.hdfHandle, cpth, ckey, (*C.long)(unsafe.Pointer(&vals[0])))
+	if st < 0 {
+		chk.Panic("cannot read attibute %q from vals in path=%q", key, path)
+	}
+	return
+}
--- io/h5/ints.go
+++ io/h5/ints.go
@@ -36,82 +36,12 @@ func (o *File) GetInts(path string) (v []int) {
 	return
 }
 
-// IntsSetAttr sets ints attibute
-func (o *File) IntsSetAttr(path, key string, vals []int) {
-	if o.useGob {
-		if o.gobReading {
-			chk.Panic("cannot put %q because file is open for READONLY", path)
-		}
-		o.gobEnc.Encode("IntsSetAttr")
-		o.gobEnc.Encode(path)
-		o.gobEnc.Encode(key)
-		o.gobEnc.Encode(vals)
-		return
-	}
-	ckey := C.CString(key)
-	defer C.free(unsafe.Pointer(ckey))
-	n := C.size_t(len(vals))
-	o.hierarchCreate(path, func(cp *C.char) C.herr_t {
-		st := C.H5LTset_attribute_long(o.hdfHandle, cp, ckey, (*C.long)(unsafe.Pointer(&vals[0])), n)
-		if st < 0 {
-			chk.Panic("cannot set attibute %q to vals in path=%q", key, path)
-		}
-		return 0
-	})
-}
-
-// IntsReadAttr reads ints attribute
-func (o *File) IntsReadAttr(path, key string) (vals []int) {
-	if o.useGob {
-		var cmd string
-		o.gobDec.Decode(&cmd)
-		if cmd != "IntsSetAttr" {
-			chk.Panic("wrong command => %q\n(r/w commands need to be called in the same order)", cmd)
-		}
-		var rpath string
-		o.gobDec.Decode(&rpath)
-		if rpath != path {
-			chk.Panic("cannot read path: %s != %s\n(r/w commands need to be called in the same order)", path, rpath)
-		}
-		var rkey string
-		o.gobDec.Decode(&rkey)
-		if rkey != key {
-			chk.Panic("cannot read key: %s != %s\n(r/w commands need to be called in the same order)", key, rkey)
-		}
-		o.gobDec.Decode(&vals)
-		return
-	}
-	o.filterPath(path)
-	cpth, ckey := C.CString(path), C.CString(key)
-	defer C.free(unsafe.Pointer(cpth))
-	defer C.free(unsafe.Pointer(ckey))
-	var rank int
-	st := C.H5LTget_attribute_ndims(o.hdfHandle, cpth, ckey, (*C.int)(unsafe.Pointer(&rank))) //unsafe.Pointer(&rank[0])))
-	if st < 0 {
-		chk.Panic("cannot read attibute %q from rank in path=%q", key, path)
-	}
-	if rank != 1 {
-		chk.Panic("cannot read attibute %q because rank == %d != 1. path=%q", key, rank, path)
-	}
-	var typeClass C.H5T_class_t
-	var typeSize C.size_t
-	dims := make([]int, rank)
-	st = C.H5LTget_attribute_info(o.hdfHandle, cpth, ckey, (*C.hsize_t)(unsafe.Pointer(&dims[0])), &typeClass, &typeSize)
-	if st < 0 {
-		chk.Panic("cannot read attibute %q from dims in path=%q", key, path)
-	}
-	vals = make([]int, dims[0])
-	st = C.H5LTget_attribute_long(o.hdfHandle, cpth, ckey, (*C.long)(unsafe.Pointer(&vals[0])))
-	if st < 0 {
-		chk.Panic("cannot read attibute %q from vals in path=%q", key, path)
-	}
-	return
-}
-
 // auxiliary methods ///////////////////////////////////////////////////////////////////////////
 
 // putInts puts an array of integers into file
 func (o *File) putInts(path string, dims []int, dat []int) {
+
+	// GOB
 	if o.useGob {
 		if o.gobReading {
 			chk.Panic("cannot put %q because file is open for READONLY", path)
@@ -123,6 +53,8 @@ func (o *File) putInts(path string, dims []int, dat []int) {
 		o.gobEnc.Encode(dat)
 		return
 	}
+
+	// HDF5
 	rnk := C.int(len(dims))
 	o.hierarchCreate(path, func(cp *C.char) C.herr_t {
 		return C.H5LTmake_dataset_long(o.hdfHandle, cp, rnk, (*C.hsize_t)(unsafe.Pointer(&dims[0])), (*C.long)(unsafe.Pointer(&dat[0])))
@@ -131,10 +63,14 @@ func (o *File) putInts(path string, dims []int, dat []int) {
 
 // putArrayIntNoGroups puts integers into file without creating groups
 func (o *File) putArrayIntNoGroups(path string, dat []int) {
+
+	// GOB
 	if o.useGob {
 		o.putInts(path, []int{len(dat)}, dat)
 		return
 	}
+
+	// HDF5
 	cpth := C.CString(path)
 	defer C.free(unsafe.Pointer(cpth))
 	dims := []int{len(dat)}
@@ -146,6 +82,8 @@ func (o *File) putArrayIntNoGroups(path string, dat []int) {
 
 // getInts gets an array of integers from file
 func (o *File) getInts(path string, ismat bool) (dims, dat []int) {
+
+	// GOB
 	if o.useGob {
 		var cmd string
 		o.gobDec.Decode(&cmd)
@@ -163,6 +101,8 @@ func (o *File) getInts(path string, ismat bool) (dims, dat []int) {
 		o.gobDec.Decode(&dat)
 		return
 	}
+
+	// HDF5
 	o.filterPath(path)
 	cpth := C.CString(path)
 	defer C.free(unsafe.Pointer(cpth))
