6e072231582e2e3f56e33dd384ec1a60b935880d
Dorival Pedroso <pedroso@cpmech.com>
2018-03-11 01:22:58 -0800

    Add tests of parameters change

--- ml/t_paramsreg_test.go
+++ ml/t_paramsreg_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/io"
 )
 
 func TestParamsReg01(tst *testing.T) {
@@ -21,20 +22,39 @@ func TestParamsReg01(tst *testing.T) {
 	params.Theta[1] = 2
 	params.Theta[2] = 3
 	params.Bias = 4
+	params.Lambda = 0.1
+	params.Degree = 3
 	chk.Array(tst, "θ", 1e-15, params.Theta, []float64{1, 2, 3})
 	chk.Float64(tst, "b", 1e-15, params.Bias, 4)
-	chk.Array(tst, "θ", 1e-15, params.thetaCopy, nil)
-	chk.Float64(tst, "b", 1e-15, params.biasCopy, 0)
+	chk.Float64(tst, "λ", 1e-15, params.Lambda, 0.1)
+	chk.Int(tst, "P", params.Degree, 3)
+	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, nil)
+	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 0)
+	chk.Float64(tst, "λcopy", 1e-15, params.biasCopy, 0)
+	chk.Int(tst, "Pcopy", params.degreeCopy, 0)
+	io.Pl()
 	params.Backup()
-	chk.Array(tst, "θ", 1e-15, params.thetaCopy, []float64{1, 2, 3})
-	chk.Float64(tst, "b", 1e-15, params.biasCopy, 4)
+	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, []float64{1, 2, 3})
+	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 4)
+	chk.Float64(tst, "λcopy", 1e-15, params.lambdaCopy, 0.1)
+	chk.Int(tst, "Pcopy", params.degreeCopy, 3)
+	io.Pl()
 	params.Theta[1] = -2
 	params.Bias = -4
-	chk.Array(tst, "θ", 1e-15, params.Theta, []float64{1, -2, 3})
-	chk.Float64(tst, "b", 1e-15, params.Bias, -4)
-	chk.Array(tst, "θ", 1e-15, params.thetaCopy, []float64{1, 2, 3})
-	chk.Float64(tst, "b", 1e-15, params.biasCopy, 4)
+	params.Lambda = 0.01
+	params.Degree = 4
+	chk.Array(tst, "θchanged", 1e-15, params.Theta, []float64{1, -2, 3})
+	chk.Float64(tst, "bchanged", 1e-15, params.Bias, -4)
+	chk.Float64(tst, "λchanged", 1e-15, params.Lambda, 0.01)
+	chk.Int(tst, "Pchanged", params.Degree, 4)
+	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, []float64{1, 2, 3})
+	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 4)
+	chk.Float64(tst, "λcopy", 1e-15, params.lambdaCopy, 0.1)
+	chk.Int(tst, "Pcopy", params.degreeCopy, 3)
+	io.Pl()
 	params.Restore()
-	chk.Array(tst, "θ", 1e-15, params.Theta, []float64{1, 2, 3})
-	chk.Float64(tst, "b", 1e-15, params.Bias, 4)
+	chk.Array(tst, "θrestored", 1e-15, params.Theta, []float64{1, 2, 3})
+	chk.Float64(tst, "brestored", 1e-15, params.Bias, 4)
+	chk.Float64(tst, "λ", 1e-15, params.Lambda, 0.1)
+	chk.Int(tst, "P", params.Degree, 3)
 }
