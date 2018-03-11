68e3ab4cf35fe53ea299eb31c84b616e4b27ea27
Dorival Pedroso <pedroso@cpmech.com>
2018-03-11 10:38:31 -0700

    Improve access to params data. Make it observable

--- ml/paramsreg.go
+++ ml/paramsreg.go
@@ -4,40 +4,131 @@
 
 package ml
 
-import "github.com/cpmech/gosl/la"
+import (
+	"github.com/cpmech/gosl/la"
+	"github.com/cpmech/gosl/utl"
+)
 
 // ParamsReg holds the θ and b parameters for regression computations
 type ParamsReg struct {
-	Theta      la.Vector // θ parameter [nFeatures]
-	Bias       float64   // bias parameter
-	Lambda     float64   // regularization parameter
-	Degree     int       // degree of polynomial
-	thetaCopy  la.Vector // copy of θ
-	biasCopy   float64   // copy of b
-	lambdaCopy float64   // copy of λ
-	degreeCopy int       // copy of degree
+	utl.Observable // notifies interested parties
+
+	// main
+	theta  la.Vector // θ parameter [nFeatures]
+	bias   float64   // bias parameter
+	lambda float64   // regularization parameter
+	degree int       // degree of polynomial
+
+	// backup
+	bkpTheta  la.Vector // copy of θ
+	bkpBias   float64   // copy of b
+	bkpLambda float64   // copy of λ
+	bkpDegree int       // copy of degree
 }
 
 // NewParamsReg returns a new object to hold regression parameters
 func NewParamsReg(nFeatures int) (o *ParamsReg) {
 	o = new(ParamsReg)
-	o.Theta = la.NewVector(nFeatures)
-	o.thetaCopy = la.NewVector(nFeatures)
+	o.theta = la.NewVector(nFeatures)
+	o.bkpTheta = la.NewVector(nFeatures)
 	return o
 }
 
 // Backup creates an internal copy of parameters
 func (o *ParamsReg) Backup() {
-	copy(o.thetaCopy, o.Theta)
-	o.biasCopy = o.Bias
-	o.lambdaCopy = o.Lambda
-	o.degreeCopy = o.Degree
+	copy(o.bkpTheta, o.theta)
+	o.bkpBias = o.bias
+	o.bkpLambda = o.lambda
+	o.bkpDegree = o.degree
 }
 
-// Restore restores an internal copy of parameters
+// Restore restores an internal copy of parameters and notifies observers
 func (o *ParamsReg) Restore() {
-	copy(o.Theta, o.thetaCopy)
-	o.Bias = o.biasCopy
-	o.Lambda = o.lambdaCopy
-	o.Degree = o.degreeCopy
+	copy(o.theta, o.bkpTheta)
+	o.bias = o.bkpBias
+	o.lambda = o.bkpLambda
+	o.degree = o.bkpDegree
+	o.NotifyUpdate()
+}
+
+// SetParams sets θ and b and notifies observers
+func (o *ParamsReg) SetParams(θ la.Vector, b float64) {
+	copy(o.theta, θ)
+	o.bias = b
+	o.NotifyUpdate()
+}
+
+// SetParam sets either θ or b (use negative indices for b). Notifies observers
+//  i -- index of θ or -1 for bias
+func (o *ParamsReg) SetParam(i int, value float64) {
+	defer o.NotifyUpdate()
+	if i < 0 {
+		o.bias = value
+		return
+	}
+	o.theta[i] = value
+}
+
+// GetParam returns either θ or b (use negative indices for b)
+//  i -- index of θ or -1 for bias
+func (o *ParamsReg) GetParam(i int) (value float64) {
+	if i < 0 {
+		return o.bias
+	}
+	return o.theta[i]
+}
+
+// SetThetas sets the whole vector θ and notifies observers
+func (o *ParamsReg) SetThetas(θ la.Vector) {
+	o.theta.Apply(1, θ)
+	o.NotifyUpdate()
+}
+
+// GetThetas gets a copy of θ
+func (o *ParamsReg) GetThetas() (θ la.Vector) {
+	return o.theta.GetCopy()
+}
+
+// SetTheta sets one component of θ and notifies observers
+func (o *ParamsReg) SetTheta(i int, θi float64) {
+	o.theta[i] = θi
+	o.NotifyUpdate()
+}
+
+// GetTheta returns the value of θ[i]
+func (o *ParamsReg) GetTheta(i int) (θi float64) {
+	return o.theta[i]
+}
+
+// SetBias sets b and notifies observers
+func (o *ParamsReg) SetBias(b float64) {
+	o.bias = b
+	o.NotifyUpdate()
+}
+
+// GetBias gets a copy of b
+func (o *ParamsReg) GetBias() (b float64) {
+	return o.bias
+}
+
+// SetLambda sets λ and notifies observers
+func (o *ParamsReg) SetLambda(λ float64) {
+	o.lambda = λ
+	o.NotifyUpdate()
+}
+
+// GetLambda gets a copy of λ
+func (o *ParamsReg) GetLambda() (λ float64) {
+	return o.lambda
+}
+
+// SetDegree sets p and notifies observers
+func (o *ParamsReg) SetDegree(p int) {
+	o.degree = p
+	o.NotifyUpdate()
+}
+
+// GetDegree gets a copy of p
+func (o *ParamsReg) GetDegree() (p int) {
+	return o.degree
 }
--- ml/t_paramsreg_test.go
+++ ml/t_paramsreg_test.go
@@ -14,47 +14,125 @@ import (
 func TestParamsReg01(tst *testing.T) {
 
 	//verbose()
-	chk.PrintTitle("ParamsReg01. parameters for regression")
+	chk.PrintTitle("ParamsReg01. Parameters for regression")
 
 	nFeatures := 3
 	params := NewParamsReg(nFeatures)
-	params.Theta[0] = 1
-	params.Theta[1] = 2
-	params.Theta[2] = 3
-	params.Bias = 4
-	params.Lambda = 0.1
-	params.Degree = 3
-	chk.Array(tst, "θ", 1e-15, params.Theta, []float64{1, 2, 3})
-	chk.Float64(tst, "b", 1e-15, params.Bias, 4)
-	chk.Float64(tst, "λ", 1e-15, params.Lambda, 0.1)
-	chk.Int(tst, "P", params.Degree, 3)
-	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, nil)
-	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 0)
-	chk.Float64(tst, "λcopy", 1e-15, params.biasCopy, 0)
-	chk.Int(tst, "Pcopy", params.degreeCopy, 0)
+	params.theta[0] = 1
+	params.theta[1] = 2
+	params.theta[2] = 3
+	params.bias = 4
+	params.lambda = 0.1
+	params.degree = 3
+	chk.Array(tst, "θ", 1e-15, params.theta, []float64{1, 2, 3})
+	chk.Float64(tst, "b", 1e-15, params.bias, 4)
+	chk.Float64(tst, "λ", 1e-15, params.lambda, 0.1)
+	chk.Int(tst, "P", params.degree, 3)
+	chk.Array(tst, "θcopy", 1e-15, params.bkpTheta, nil)
+	chk.Float64(tst, "bcopy", 1e-15, params.bkpBias, 0)
+	chk.Float64(tst, "λcopy", 1e-15, params.bkpBias, 0)
+	chk.Int(tst, "Pcopy", params.bkpDegree, 0)
 	io.Pl()
 	params.Backup()
-	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, []float64{1, 2, 3})
-	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 4)
-	chk.Float64(tst, "λcopy", 1e-15, params.lambdaCopy, 0.1)
-	chk.Int(tst, "Pcopy", params.degreeCopy, 3)
+	chk.Array(tst, "θcopy", 1e-15, params.bkpTheta, []float64{1, 2, 3})
+	chk.Float64(tst, "bcopy", 1e-15, params.bkpBias, 4)
+	chk.Float64(tst, "λcopy", 1e-15, params.bkpLambda, 0.1)
+	chk.Int(tst, "Pcopy", params.bkpDegree, 3)
 	io.Pl()
-	params.Theta[1] = -2
-	params.Bias = -4
-	params.Lambda = 0.01
-	params.Degree = 4
-	chk.Array(tst, "θchanged", 1e-15, params.Theta, []float64{1, -2, 3})
-	chk.Float64(tst, "bchanged", 1e-15, params.Bias, -4)
-	chk.Float64(tst, "λchanged", 1e-15, params.Lambda, 0.01)
-	chk.Int(tst, "Pchanged", params.Degree, 4)
-	chk.Array(tst, "θcopy", 1e-15, params.thetaCopy, []float64{1, 2, 3})
-	chk.Float64(tst, "bcopy", 1e-15, params.biasCopy, 4)
-	chk.Float64(tst, "λcopy", 1e-15, params.lambdaCopy, 0.1)
-	chk.Int(tst, "Pcopy", params.degreeCopy, 3)
+	params.theta[1] = -2
+	params.bias = -4
+	params.lambda = 0.01
+	params.degree = 4
+	chk.Array(tst, "θchanged", 1e-15, params.theta, []float64{1, -2, 3})
+	chk.Float64(tst, "bchanged", 1e-15, params.bias, -4)
+	chk.Float64(tst, "λchanged", 1e-15, params.lambda, 0.01)
+	chk.Int(tst, "Pchanged", params.degree, 4)
+	chk.Array(tst, "θcopy", 1e-15, params.bkpTheta, []float64{1, 2, 3})
+	chk.Float64(tst, "bcopy", 1e-15, params.bkpBias, 4)
+	chk.Float64(tst, "λcopy", 1e-15, params.bkpLambda, 0.1)
+	chk.Int(tst, "Pcopy", params.bkpDegree, 3)
 	io.Pl()
 	params.Restore()
-	chk.Array(tst, "θrestored", 1e-15, params.Theta, []float64{1, 2, 3})
-	chk.Float64(tst, "brestored", 1e-15, params.Bias, 4)
-	chk.Float64(tst, "λ", 1e-15, params.Lambda, 0.1)
-	chk.Int(tst, "P", params.Degree, 3)
+	chk.Array(tst, "θrestored", 1e-15, params.theta, []float64{1, 2, 3})
+	chk.Float64(tst, "brestored", 1e-15, params.bias, 4)
+	chk.Float64(tst, "λ", 1e-15, params.lambda, 0.1)
+	chk.Int(tst, "P", params.degree, 3)
+}
+
+type observerT struct {
+	name   string
+	params *ParamsReg
+	msg    string
+}
+
+func (o *observerT) Name() string {
+	return o.name
+}
+
+func (o *observerT) Update() {
+	o.msg = io.Sf("got θ=%.1f b=%.1f λ=%.3f p=%1d", o.params.GetThetas(), o.params.GetBias(), o.params.GetLambda(), o.params.GetDegree())
+}
+
+func TestParamsReg02(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("ParamsReg02. Parameters for regression. Observable")
+
+	// parameters
+	nFeatures := 3
+	params := NewParamsReg(nFeatures)
+
+	// observer
+	observer := &observerT{"obs01", params, "nothing here yet"}
+	params.AddObserver(observer)
+	chk.String(tst, observer.msg, "nothing here yet")
+
+	// check SetParams
+	io.Pl()
+	params.SetParams([]float64{1, 2, 3}, 4)
+	chk.Array(tst, "GetThetas", 1e-15, params.GetThetas(), []float64{1, 2, 3})
+	chk.Float64(tst, "GetBias", 1e-15, params.GetBias(), 4)
+	chk.Float64(tst, "GetLambda", 1e-15, params.GetLambda(), 0)
+	chk.Int(tst, "GetDegree", params.GetDegree(), 0)
+	chk.String(tst, observer.msg, "got θ=[1.0 2.0 3.0] b=4.0 λ=0.000 p=0")
+
+	// check SetParam
+	io.Pl()
+	params.SetParam(0, -1)
+	params.SetParam(-1, -4)
+	chk.Float64(tst, "GetTheta[0]", 1e-15, params.GetTheta(0), -1)
+	chk.Float64(tst, "GetBias", 1e-15, params.GetBias(), -4)
+	chk.Float64(tst, "θ=GetParam(0)", 1e-15, params.GetParam(0), -1)
+	chk.Float64(tst, "b=GetParam(-1)", 1e-15, params.GetParam(-1), -4)
+	chk.String(tst, observer.msg, "got θ=[-1.0 2.0 3.0] b=-4.0 λ=0.000 p=0")
+
+	// check SetThetas
+	io.Pl()
+	params.SetThetas([]float64{-3, -2, -1})
+	chk.Array(tst, "GetThetas", 1e-15, params.GetThetas(), []float64{-3, -2, -1})
+	chk.String(tst, observer.msg, "got θ=[-3.0 -2.0 -1.0] b=-4.0 λ=0.000 p=0")
+
+	// check SetTheta
+	io.Pl()
+	params.SetTheta(0, -33)
+	chk.Float64(tst, "GetTheta[0]", 1e-15, params.GetTheta(0), -33)
+	chk.String(tst, observer.msg, "got θ=[-33.0 -2.0 -1.0] b=-4.0 λ=0.000 p=0")
+
+	// check SetBias
+	io.Pl()
+	params.SetBias(123)
+	chk.Float64(tst, "GetBias", 1e-15, params.GetBias(), 123)
+	chk.String(tst, observer.msg, "got θ=[-33.0 -2.0 -1.0] b=123.0 λ=0.000 p=0")
+
+	// check SetLambda
+	io.Pl()
+	params.SetLambda(0.01)
+	chk.Float64(tst, "GetLambda", 1e-15, params.GetLambda(), 0.01)
+	chk.String(tst, observer.msg, "got θ=[-33.0 -2.0 -1.0] b=123.0 λ=0.010 p=0")
+
+	// check SetDegree
+	io.Pl()
+	params.SetDegree(8)
+	chk.Int(tst, "GetDegree", params.GetDegree(), 8)
+	chk.String(tst, observer.msg, "got θ=[-33.0 -2.0 -1.0] b=123.0 λ=0.010 p=8")
 }
