f74184b18446cad427319d39ac71bc375272a34f
Dorival Pedroso <pedroso@cpmech.com>
2018-03-27 16:25:56 -0700

    Remove some GCC flags because Travis doesn't like them (io/h5 package)

--- io/h5/flags.go
+++ io/h5/flags.go
@@ -7,7 +7,7 @@
 package h5
 
 /*
-#cgo CFLAGS: -I/usr/include/hdf5/serial -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -Wdate-time -D_FORTIFY_SOURCE=2 -g -O2 -fstack-protector-strong -Wformat -Werror=format-security
+#cgo CFLAGS: -I/usr/include/hdf5/serial -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -D_FORTIFY_SOURCE=2 -g -O2 -Wformat -Werror=format-security
 #cgo LDFLAGS: -L/usr/lib/x86_64-linux-gnu/hdf5/serial /usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5_hl.a /usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.a -Wl,-Bsymbolic-functions -Wl,-z,relro -lpthread -lsz -lz -ldl -lm -Wl,-rpath -Wl,/usr/lib/x86_64-linux-gnu/hdf5/serial
 */
 import "C"
