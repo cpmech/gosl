1a1b2119b8b7fdad9e664f23afc5f9b29bd63a83
Dorival Pedroso <dorival.pedroso@gmail.com>
2018-09-05 17:45:56 +1000

    Add flags to compile MKL on macos

--- la/mkl/flags.go
+++ la/mkl/flags.go
@@ -5,7 +5,10 @@
 package mkl
 
 /*
-#cgo CFLAGS: -O2 -DMKL_ILP64 -I/opt/intel/mkl/include
-#cgo LDFLAGS: -Wl,--start-group /opt/intel/mkl/lib/intel64/libmkl_intel_ilp64.a /opt/intel/mkl/lib/intel64/libmkl_intel_thread.a /opt/intel/mkl/lib/intel64/libmkl_core.a /opt/intel/lib/intel64/libiomp5.a -Wl,--end-group -lpthread -lm -ldl -L/opt/intel/lib/intel64
+#cgo linux CFLAGS: -O2 -DMKL_ILP64 -I/opt/intel/mkl/include
+#cgo linux LDFLAGS: -Wl,--start-group /opt/intel/mkl/lib/intel64/libmkl_intel_ilp64.a /opt/intel/mkl/lib/intel64/libmkl_intel_thread.a /opt/intel/mkl/lib/intel64/libmkl_core.a /opt/intel/lib/intel64/libiomp5.a -Wl,--end-group -lpthread -lm -ldl -L/opt/intel/lib/intel64
+
+#cgo darwin CFLAGS: -O2 -DMKL_ILP64 -m64 -I/opt/intel/mkl/include
+#cgo darwin LDFLAGS: -L/opt/intel/lib /opt/intel/mkl/lib/libmkl_intel_ilp64.a /opt/intel/mkl/lib/libmkl_intel_thread.a /opt/intel/mkl/lib/libmkl_core.a /opt/intel/lib/libiomp5.a -lpthread -lm -ldl
 */
 import "C"
