5c00666dc852e914c36e3a49ed526b4d632d0d4a
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-09-16 09:14:18 +0000

    Add no-cache to docker build

--- zscripts/dk-build-image.bash
+++ zscripts/dk-build-image.bash
@@ -9,5 +9,5 @@ echo "... docker .................................................."
 echo "............................................................."
 echo
 
-docker build -t gosl/$NAME:$VERSION . --build-arg GOSL_VERSION=$VERSION
+docker build --no-cache -t gosl/$NAME:$VERSION . --build-arg GOSL_VERSION=$VERSION
 docker images -q -f "dangling=true" | xargs docker rmi
